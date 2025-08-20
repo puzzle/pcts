f10c68a220a3bcb9e3146f4d42e2637a63c7358b
schiltpzzle <schilt@puzzle.ch>
2025-08-20 09:19:39 +0200

    feat: Initialize project UI and configuration #51

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,17 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import "@puzzleitc/puzzle-shell";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+})
+export class App {
+  protected title = 'pcts';
+}
+
