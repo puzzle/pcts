cc66367e63e9a23e8f524bfcaebe976e971f1716
Miguel Lehmann <lehmann@puzzle.ch>
2025-08-22 16:23:37 +0200

    feat: Configure Jest #7

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent,
+        RouterModule.forRoot([])]
     })
       .compileComponents();
   });
@@ -14,13 +16,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app)
       .toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent)
-      .toContain('Hello, pcts');
   });
 });
