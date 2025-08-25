3b2e3d1fee1bc632b6c0d9fe25a55c02b2266fe3
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-08-25 08:46:14 +0200

    feat: implement flyway and create migrations  #30

---
+++ backend/src/main/resources/db/dev-data-migration/V1_1_0__initialData.sql
@@ -0,0 +1,14 @@
+truncate table example cascade;
+
+
+insert into example (id, text)
+values (1, 'Example 1'),
+       (11, 'Example 2'),
+       (21, 'Example 3'),
+       (31, 'Example 4'),
+       (41, 'Example 5');
+
+
+
+
+
