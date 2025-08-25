8734e9923335b2f31c23507a93f6b3f6ef0dfa1f
Miguel Lehmann <lehmann@puzzle.ch>
2025-08-25 08:01:36 +0200

    ci: Add a GitHub workflow for frontend testing #7

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+jest.mock('@puzzleitc/puzzle-shell', () => jest.fn());
 
 describe('AppComponent', () => {
   beforeEach(async() => {
