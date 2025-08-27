43c420d62380c70d40fae34f0126040eacaf4536
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-08-27 11:55:23 +0200

    feat: make pcts zoneless  #63

--- frontend/src/app/example/example.component.ts
+++ frontend/src/app/example/example.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal, computed } from '@angular/core';
 import { ExampleService } from './example.service';
 import { Observable } from 'rxjs';
 import { ExampleDto } from './dto/example.dto';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-example',
-  imports: [CommonModule],
+  imports: [CommonModule,
+    FormsModule],
   templateUrl: './example.component.html',
   styleUrl: './example.component.css'
 })
 export class ExampleComponent implements OnInit {
   private service = inject(ExampleService);
+
+  exampleId = signal<number>(1);
+
+  example = computed(() => this.service.getExampleById(this.exampleId()));
 
   examples$!: Observable<ExampleDto[]>;
 
