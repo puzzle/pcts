a32bb0d08eb764169a556aa0d0a4de311390be02
Livio Canobbio <canobbio@puzzle.ch>
2025-09-05 11:50:17 +0200

    refactor: add attribute isManagement to role #34

--- backend/src/main/java/ch/puzzle/pcts/dto/role/RoleDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/role/RoleDto.java
@@ -2,5 +2,5 @@ package ch.puzzle.pcts.dto.role;
 
 import java.time.LocalDateTime;
 
-public record RoleDto(Long id, String name, LocalDateTime deleted_at) {
+public record RoleDto(Long id, String name, LocalDateTime deleted_at, boolean isManagement) {
 }
