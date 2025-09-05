30ca498316180c450b0e180863c33aa5cac34dfb
Livio Canobbio <canobbio@puzzle.ch>
2025-09-05 11:48:00 +0200

    feat: add role migration #34

---
+++ backend/src/main/resources/db/migration/V0_0_3__role.sql
@@ -0,0 +1,7 @@
+CREATE SEQUENCE IF NOT EXISTS sequence_role;
+CREATE TABLE IF NOT EXISTS role
+(
+    id          BIGINT NOT NUll PRIMARY KEY,
+    name        VARCHAR(250) NOT NULL,
+    deleted_at  TIMESTAMP NOT NULL
+);
