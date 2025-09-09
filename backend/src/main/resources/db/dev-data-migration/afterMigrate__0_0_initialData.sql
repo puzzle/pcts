ffc63a5e9745efc9bc0986540313a88aef9aadfc
Livio Canobbio <canobbio@puzzle.ch>
2025-09-09 16:23:32 +0200

    fix: rename file like okr and change id assigment #34

--- backend/src/main/resources/db/dev-data-migration/afterMigrate__0_0_initialData.sql
+++ backend/src/main/resources/db/dev-data-migration/afterMigrate__0_0_initialData.sql
@@ -13,8 +13,10 @@ TRUNCATE TABLE role CASCADE;
 
 INSERT INTO role (id, name, deleted_at, is_management)
 VALUES
-    (nextval('sequence_role'), 'Administrator', '1970-01-01 00:00:00', TRUE),
-    (nextval('sequence_role'), 'Manager', null, TRUE),
-    (nextval('sequence_role'), 'Team Lead', null, TRUE),
-    (nextval('sequence_role'), 'Developer', null, FALSE),
-    (nextval('sequence_role'), 'Intern', null, FALSE);
+    (1, 'Administrator', '1970-01-01 00:00:00', TRUE),
+    (2, 'Manager', null, TRUE),
+    (3, 'Team Lead', null, TRUE),
+    (4, 'Developer', null, FALSE),
+    (5, 'Intern', null, FALSE);
+
+SELECT setval('sequence_role', (SELECT MAX(id) FROM role));
