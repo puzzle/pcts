06886ecfaec8d7c3c9aba0b2cb28a2d9a7ac2932
Livio Canobbio <canobbio@puzzle.ch>
2025-09-09 15:52:07 +0200

    fix: change datatype from varchar to text #34

--- backend/src/main/resources/db/migration/V0_0_2__role.sql
+++ backend/src/main/resources/db/migration/V0_0_2__role.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE IF NOT EXISTS sequence_role;
 CREATE TABLE IF NOT EXISTS role
 (
     id              BIGINT NOT NUll PRIMARY KEY,
-    name            VARCHAR(250) NOT NULL,
+    name            TEXT NOT NULL,
     deleted_at      TIMESTAMP,
     is_management   BOOLEAN NOT NULL
 );
