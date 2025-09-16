8df9a826f828f2f8e410ec91758d10972e331291
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-09-16 15:16:41 +0200

    test: test application availability  #28

---
+++ backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
@@ -0,0 +1,48 @@
+package ch.puzzle.pcts;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+class ApplicationAvailabilityTest {
+
+    @Autowired
+    private ApplicationAvailability applicationAvailability;
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @DisplayName("Should accept traffic")
+    @Test
+    void shouldAcceptTraffic() {
+        assertThat(applicationAvailability.getReadinessState()).isEqualTo(ReadinessState.ACCEPTING_TRAFFIC);
+    }
+
+    @DisplayName("Should be live")
+    @Test
+    void shouldBeLive() {
+        assertThat(applicationAvailability.getLivenessState()).isEqualTo(LivenessState.CORRECT);
+    }
+}
