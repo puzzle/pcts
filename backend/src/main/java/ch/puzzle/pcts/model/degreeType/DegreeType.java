e62ae8c50546c6c47f17aad835659e70eb764532
Livio Canobbio <canobbio@puzzle.ch>
2025-09-16 13:12:16 +0200

    feat: add deegree type model dto mapper and repo #38

---
+++ backend/src/main/java/ch/puzzle/pcts/model/degreeType/DegreeType.java
@@ -0,0 +1,74 @@
+package ch.puzzle.pcts.model.degreeType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class DegreeType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal highlyRelevantPoints;
+
+    private BigDecimal limitedRelevantPoints;
+
+    private BigDecimal littleRelevantPoints;
+
+    public DegreeType(Long id, String name, BigDecimal highlyRelevantPoints, BigDecimal limitedRelevantPoints,
+                      BigDecimal littleRelevantPoints) {
+        this.id = id;
+        this.name = name;
+        this.highlyRelevantPoints = highlyRelevantPoints;
+        this.limitedRelevantPoints = limitedRelevantPoints;
+        this.littleRelevantPoints = littleRelevantPoints;
+    }
+
+    public DegreeType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getHighlyRelevantPoints() {
+        return highlyRelevantPoints;
+    }
+
+    public void setHighlyRelevantPoints(BigDecimal highlyRelevantPoints) {
+        this.highlyRelevantPoints = highlyRelevantPoints;
+    }
+
+    public BigDecimal getLimitedRelevantPoints() {
+        return limitedRelevantPoints;
+    }
+
+    public void setLimitedRelevantPoints(BigDecimal limitedRelevantPoints) {
+        this.limitedRelevantPoints = limitedRelevantPoints;
+    }
+
+    public BigDecimal getLittleRelevantPoints() {
+        return littleRelevantPoints;
+    }
+
+    public void setLittleRelevantPoints(BigDecimal littleRelevantPoints) {
+        this.littleRelevantPoints = littleRelevantPoints;
+    }
+}
