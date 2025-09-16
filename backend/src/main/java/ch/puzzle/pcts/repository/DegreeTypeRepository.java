b3a270e1dfb9175ede63d320cfba7a7a93d94ed0
Livio Canobbio <canobbio@puzzle.ch>
2025-09-16 16:40:52 +0200

    feat: not finished servies and add name dto #38

--- backend/src/main/java/ch/puzzle/pcts/repository/DegreeTypeRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/DegreeTypeRepository.java
@@ -1,9 +1,16 @@
 package ch.puzzle.pcts.repository;
 
+import ch.puzzle.pcts.dto.degreeType.DegreeTypeNameDto;
 import ch.puzzle.pcts.model.degreeType.DegreeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DegreeTypeRepository extends JpaRepository<DegreeType, Long> {
+
+    @Query("SELECT id, name FROM DegreeType")
+    List<DegreeTypeNameDto> findAllNames();
 }
--- backend/src/main/java/ch/puzzle/pcts/service/business/DegreeTypeBusinessService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/business/DegreeTypeBusinessService.java
@@ -1,7 +1,13 @@
 package ch.puzzle.pcts.service.business;
 
+import ch.puzzle.pcts.dto.degreeType.DegreeTypeNameDto;
+import ch.puzzle.pcts.exception.PCTSException;
+import ch.puzzle.pcts.model.degreeType.DegreeType;
+import ch.puzzle.pcts.model.error.ErrorKey;
 import ch.puzzle.pcts.service.persistence.DegreeTypePersistenceService;
 import ch.puzzle.pcts.service.validation.DegreeTypeValidationService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +19,37 @@ public class DegreeTypeBusinessService {
                                      DegreeTypePersistenceService persistenceService) {
         this.validationService = validationService;
         this.persistenceService = persistenceService;
+    }
+
+    public List<DegreeType> getAll() {
+        return persistenceService.getAll();
+    }
+
+    public DegreeType create(DegreeType degreeType) {
+        validationService.validateOnCreate(degreeType);
+        return persistenceService.create(degreeType);
+    }
+
+    public List<DegreeTypeNameDto> getAllNames() {
+        return persistenceService.getAllNames();
+    }
+
+    public DegreeType getById(Long id) {
+        validationService.validateOnGetById(id);
+        return persistenceService
+                .getById(id)
+                .orElseThrow(() -> new PCTSException(HttpStatus.NOT_FOUND,
+                                                     "Degree type with id: " + id + " does not exist.",
+                                                     ErrorKey.NOT_FOUND));
+    }
+
+    public DegreeType update(Long id, DegreeType degreeType) {
+        validationService.validateOnUpdate(id, degreeType);
+        return persistenceService.update(id, degreeType);
+    }
+
+    public void delete(Long id) {
+        validationService.validateOnDelete(id);
+        persistenceService.delete(id);
     }
 }
