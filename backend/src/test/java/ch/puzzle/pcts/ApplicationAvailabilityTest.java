518985626cc84d832ed058eaed04c97d5f163683
nevio18324 <141240169+nevio18324@users.noreply.github.com>
2025-09-17 11:33:17 +0200

    feat: Add readiness and liveness endpoints #28
    * feat: add readiness and liveness endpoints to actuator  #28
    
    * feat: show health detalis when authorized  #28
    
    * test: test application availability  #28
    
    * feat: add actuator endpoint to swagger  28
    
    * feat: add favicon to backend  #28
    
    * test: test routes of actuator  #28
    
    * test: adding missing tests or correcting existing tests  #28
    
    * refactor: remove double semicolon  #28
    
    * test: add test for health endpoint  #28

---
+++ backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
@@ -0,0 +1,66 @@
+package ch.puzzle.pcts;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Testcontainers
+class ApplicationAvailabilityTest {
+    private static final String BASEURL = "/actuator/health/";
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+    @Autowired
+    private MockMvc mvc;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @DisplayName("Should be healthy")
+    @Test
+    void shouldBeHealthy() throws Exception {
+        mvc
+                .perform(get(BASEURL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"))
+                .andExpect(content().string("{\"status\":\"UP\",\"groups\":[\"liveness\",\"readiness\"]}"));
+    }
+
+    @DisplayName("Should accept traffic")
+    @Test
+    void shouldAcceptTraffic() throws Exception {
+        mvc
+                .perform(get(BASEURL + "readiness"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"))
+                .andExpect(content().string("{\"status\":\"UP\"}"));
+    }
+
+    @DisplayName("Should be live")
+    @Test
+    void shouldBeLive() throws Exception {
+        mvc
+                .perform(get(BASEURL + "liveness"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"))
+                .andExpect(content().string("{\"status\":\"UP\"}"));
+    }
+}
