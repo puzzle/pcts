7f17fc2faf672f686c571b97e9073b8a6b62b0b2
Miguel Lehmann <lehmann@puzzle.ch>
2025-09-22 08:54:37 +0200

    feat: Add component for list UI #52

---
+++ frontend/src/app/features/member/overview/member-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-overview.component',
+  imports: [],
+  templateUrl: './member-overview.component.html',
+  styleUrl: './member-overview.component.css'
+})
+export class MemberOverviewComponent {
+
+}
