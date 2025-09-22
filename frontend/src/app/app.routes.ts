f47b336b890dfd0a7c5590644806b4d848ae76a9
Miguel Lehmann <lehmann@puzzle.ch>
2025-09-22 08:54:37 +0200

    feat: Add component for list UI #52

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
+import { ExampleComponent } from './features/example/example.component';
 
 export const routes: Routes = [{
   path: 'example',
