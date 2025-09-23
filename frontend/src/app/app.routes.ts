b1e8343db6a5cf4ee9aa1c7c05bd2e216b3f39ee
Miguel Lehmann <lehmann@puzzle.ch>
2025-09-23 09:03:02 +0200

    feat: Add a list UI with filters #52

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ExampleComponent } from './features/example/example.component';
+import { MemberOverviewComponent } from './features/member/overview/member-overview.component';
 
 export const routes: Routes = [{
   path: 'example',
   component: ExampleComponent
+},
+{
+  path: '',
+  component: MemberOverviewComponent
 }];
