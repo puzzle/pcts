0c3463464929b455085bbe291e668ba7c0db5d76
Livio Canobbio <canobbio@puzzle.ch>
2025-09-24 13:01:51 +0200

    chore: add annotation to persistance service #38

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/DegreeTypePersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/DegreeTypePersistenceService.java
@@ -3,6 +3,7 @@ package ch.puzzle.pcts.service.persistence;
 import ch.puzzle.pcts.model.degree_type.DegreeType;
 import ch.puzzle.pcts.repository.DegreeTypeRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ public class DegreeTypePersistenceService {
         return repository.findAll();
     }
 
+    @Transactional
     public DegreeType create(DegreeType degreeType) {
         DegreeType createdDegreeType = repository.saveAndFlush(degreeType);
         entityManager.refresh(createdDegreeType);
@@ -34,6 +36,7 @@ public class DegreeTypePersistenceService {
         return repository.findById(id);
     }
 
+    @Transactional
     public DegreeType update(Long id, DegreeType degreeType) {
         degreeType.setId(id);
         DegreeType updatedDegreeType = repository.saveAndFlush(degreeType);
