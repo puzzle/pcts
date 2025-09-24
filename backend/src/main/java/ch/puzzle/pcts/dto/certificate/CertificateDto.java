b5042578139c3c129bceff3d75735a88888ccdf7
Manuel <moeri@puzzle.ch>
2025-09-24 10:26:59 +0200

    feat(backend): Setup tag table and functionality  #36

--- backend/src/main/java/ch/puzzle/pcts/dto/certificate/CertificateDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/certificate/CertificateDto.java
@@ -2,10 +2,12 @@ package ch.puzzle.pcts.dto.certificate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
+import java.util.List;
 
 public record CertificateDto(
         @Schema(description = "The unique identifier of certificate.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_ONLY) Long id,
         @Schema(description = "The name of the certificate.", example = "Bachelor of Sciences", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) String name,
         @Schema(description = "The amount of points the certificate provides", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) BigDecimal points,
-        @Schema(description = "A optional comment for the certificate", example = "This is a awesome certificate!", requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true) String comment) {
+        @Schema(description = "A optional comment for the certificate", example = "This is a awesome certificate!", requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true) String comment,
+        @Schema(description = "Tags that provide additional information about the certificate", example = "[\"Important Tag\", \"GitLab\", \"Exam\"]", requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true, minLength = 1) List<String> tags) {
 }
