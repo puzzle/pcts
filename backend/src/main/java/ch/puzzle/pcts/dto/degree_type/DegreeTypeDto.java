a360c9e7e93c0673e1beccff07844bb877e66426
Livio Canobbio <canobbio@puzzle.ch>
2025-09-24 13:26:28 +0200

    chore: change the dto description and add some decimal places #38

--- backend/src/main/java/ch/puzzle/pcts/dto/degree_type/DegreeTypeDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/degree_type/DegreeTypeDto.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 
 public record DegreeTypeDto(
-        @Schema(description = "The unique identifier of degree-type.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_ONLY) Long id,
-        @Schema(description = "The name of the degree-type.", example = "Bachelor's Degree", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) String name,
-        @Schema(description = "The amount of highly relevant points the degree-type provides", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) BigDecimal highlyRelevantPoints,
-        @Schema(description = "The amount of limited relevant points the degree-type provides", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) BigDecimal limitedRelevantPoints,
-        @Schema(description = "The amount of little relevant points the degree-type provides", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) BigDecimal littleRelevantPoints) {
+        @Schema(description = "The unique identifier of the degree-type.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, accessMode = Schema.AccessMode.READ_ONLY) Long id,
+        @Schema(description = "The name of the degree-type.", example = "Management", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minLength = 1) String name,
+        @Schema(description = "The amount of points of the degree-type if the degree has high relevancy.", example = "10", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minimum = "0") BigDecimal highlyRelevantPoints,
+        @Schema(description = "The amount of points of the degree-type if the degree has limited relevancy.", example = "3", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minimum = "0") BigDecimal limitedRelevantPoints,
+        @Schema(description = "The amount of points of the degree-type if the degree has little relevancy.", example = "8", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, minimum = "0") BigDecimal littleRelevantPoints) {
 }
