a71ddfdf018fec41549314a1130fa59ea8387523
schiltpzzle <schilt@puzzle.ch>
2025-09-24 08:50:07 +0200

    feat: add member form #53

--- frontend/src/app/features/member/member.service.ts
+++ frontend/src/app/features/member/member.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MemberModel } from './member.model';
 import { Observable, of } from 'rxjs';
+import { OrganisationUnit } from './dto/organisation-unit.model';
 import { EmploymentState } from '../../shared/enum/employment-state.enum';
 
 @Injectable({
@@ -62,5 +63,48 @@ export class MemberService {
         }
       }
     ]);
+  getAllOrganisationUnits(): Observable<OrganisationUnit[]> {
+    return of([
+      {
+        id: 1,
+        name: '/mobility'
+      },
+      {
+        id: 2,
+        name: '/bbt'
+      },
+      {
+        id: 3,
+        name: '/sys'
+      },
+      {
+        id: 4,
+        name: '/mem'
+      }
+    ]);
+  }
+
+  getMemberById(id: number): Observable<Member> {
+    return of({
+      id: 1,
+      name: 'Ja',
+      last_name: 'Morant',
+      birthday: new Date(),
+      abbreviation: 'JM',
+      employment_state: EmploymentState.MEMBER,
+      date_of_hire: new Date(),
+      is_admin: false,
+      organisation_unit: {
+        id: 4,
+        name: '/mem'
+      }
+    });
+  }
+
+  addMember(member: Member): Observable<Member> {
+    console.log(member);
+    return of({
+      ...member
+    });
   }
 }
