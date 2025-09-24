1bc6db5073dd2cf2d17941d6f624b0e1e3331c2e
schiltpzzle <schilt@puzzle.ch>
2025-09-24 08:50:07 +0200

    feat: add member form #53

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { MemberFormComponent } from './features/member/form/member-form.component';
 import { MemberOverviewComponent } from './features/member/overview/member-overview.component';
 import { MemberOverviewResolver } from './features/member/overview/member-overview-resolver';
 
@@ -6,4 +7,11 @@ export const routes: Routes = [{
   path: '',
   component: MemberOverviewComponent,
   resolve: { filters: MemberOverviewResolver }
+},
+{
+  path: 'member',
+  children: [{ path: 'add',
+    component: MemberFormComponent },
+  { path: 'edit/:id',
+    component: MemberFormComponent }]
 }];
--- frontend/src/app/features/member/member.service.ts
+++ frontend/src/app/features/member/member.service.ts
@@ -1,7 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { MemberModel } from './member.model';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { OrganisationUnit } from './dto/organisation-unit.model';
+import { EmploymentState } from '../../shared/enum/employment-state.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,49 @@ export class MemberService {
 
   getAllMembers(): Observable<MemberModel[]> {
     return this.httpClient.get<MemberModel[]>(this.API_URL);
+  }
+
+  getAllOrganisationUnits(): Observable<OrganisationUnit[]> {
+    return of([
+      {
+        id: 1,
+        name: '/mobility'
+      },
+      {
+        id: 2,
+        name: '/bbt'
+      },
+      {
+        id: 3,
+        name: '/sys'
+      },
+      {
+        id: 4,
+        name: '/mem'
+      }
+    ]);
+  }
+
+  getMemberById(id: number): Observable<MemberModel> {
+    return of({
+      id: 1,
+      name: 'Ja',
+      lastName: 'Morant',
+      birthDate: new Date(),
+      abbreviation: 'JM',
+      employmentState: EmploymentState.MEMBER,
+      dateOfHire: new Date(),
+      organisationUnit: {
+        id: 4,
+        name: '/mem'
+      }
+    });
+  }
+
+  addMember(member: MemberModel): Observable<MemberModel> {
+    console.log(member);
+    return of({
+      ...member
+    });
   }
 }
