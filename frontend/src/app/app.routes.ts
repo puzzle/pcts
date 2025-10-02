272da7493970a6243c8b6baa20b63ea8c0ac1c17
schiltpzzle <schilt@puzzle.ch>
2025-10-02 08:54:34 +0200

    feat: Add error component and id resolver

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,24 +1,25 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
+import { ExampleComponent } from './features/example/example.component';
 import { MemberFormComponent } from './features/member/form/member-form.component';
+import { idResolver } from './features/member/form/id-resolver.service';
 import { ExampleComponent } from './features/example/example.component';
 import { MemberOverviewComponent } from './features/member/overview/member-overview.component';
 import { MemberOverviewResolver } from './features/member/overview/member-overview-resolver';
 
-export const routes: Routes = [{
-  path: 'example',
-  component: ExampleComponent
-},
-{
-  path: '',
-  component: MemberOverviewComponent,
-  resolve: { filters: MemberOverviewResolver }
 export const routes: Routes = [{ path: 'example',
   component: ExampleComponent },
-{
-  path: 'member',
+  {
+    path: '',
+    component: MemberOverviewComponent,
+    resolve: {filters: MemberOverviewResolver}
+  },
+  {
+    path: 'member',
   children: [{ path: 'add',
     component: MemberFormComponent },
-  { path: 'edit/:id',
-    component: MemberFormComponent }]
+  { path: ':id/edit',
+    component: MemberFormComponent,
+    resolve: {
+      id: idResolver
+    } }]
 }];
--- frontend/src/app/features/member/dto/organisation-unit.model.ts
+++
@@ -1,4 +0,0 @@
-export interface OrganisationUnit {
-  id: number;
-  name: string;
-}
