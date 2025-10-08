ff97da1b806135c5914194c8927277b4a26f1c32
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-10-08 15:37:12 +0200

    test: implement persistence base to persistence service integration tests #108

--- backend/src/test/java/ch/puzzle/pcts/service/persistence/CertificatePersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/pcts/service/persistence/CertificatePersistenceServiceIT.java
@@ -1,129 +1,55 @@
 package ch.puzzle.pcts.service.persistence;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import ch.puzzle.pcts.model.certificate.Certificate;
-import ch.puzzle.pcts.model.certificate.CertificateType;
 import ch.puzzle.pcts.model.certificate.Tag;
-import jakarta.transaction.Transactional;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import ch.puzzle.pcts.repository.CertificateRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers
-@ActiveProfiles("test")
-class CertificatePersistenceServiceIT {
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CertificatePersistenceServiceIT
+        extends
+        PersistenceBaseIT<Certificate, CertificateRepository, CertificatePersistenceService> {
+
+    private CertificatePersistenceService service;
+
+    CertificatePersistenceServiceIT(CertificatePersistenceService service) {
+        super(service);
+        this.service = service;
     }
 
-    @Autowired
-    private CertificatePersistenceService persistenceService;
-
-    @Autowired
-    private TagPersistenceService tagPersistenceService;
-
-    @DisplayName("Should establish DB connection")
-    @Test
-    @Order(0)
-    void shouldEstablishConnection() {
-        assertThat(postgres.isRunning()).isTrue();
+    @Override
+    Certificate getCreateEntity() {
+        return new Certificate(null,
+                "Created certificate",
+                BigDecimal.valueOf(3),
+                "This is a newly created certificate",
+                Set.of(new Tag(1L, "Important tag"), new Tag(2L, "Way more important tag")));
     }
 
-    @DisplayName("Should get certificate and leadership experience by id")
-    @Test
-    @Order(1)
-    void shouldGetById() {
-        Optional<Certificate> certificate = persistenceService.getById(3L);
-
-        assertThat(certificate).isPresent();
-        assertThat(certificate.get().getId()).isEqualTo(3L);
-        assertThat(certificate.get().getName()).isEqualTo("Certificate 3");
-        assertThat(certificate.get().getPoints()).isEqualByComparingTo(BigDecimal.valueOf(3));
-        assertThat(certificate.get().getComment()).isEqualTo("This is Certificate 3");
-        assertThat(certificate.get().getCertificateType()).isEqualTo(CertificateType.CERTIFICATE);
-
+    @Override
+    Certificate getUpdateEntity() {
+        return new Certificate(null,
+                "Updated certificate",
+                BigDecimal.valueOf(3),
+                "This is a updated certificate",
+                Set.of(new Tag(null, "Important tag"), new Tag(null, "Way more important tag")));
     }
 
-    @DisplayName("Should get all certificates")
-    @Test
-    @Order(1)
-    void shouldGetAllCertificates() {
-        List<Certificate> all = persistenceService.getAllCertificates();
-
-        assertThat(all).hasSize(4);
-        assertThat(all)
-                .extracting(Certificate::getName)
-                .containsExactlyInAnyOrder("Certificate 1", "Certificate 2", "Certificate 3", "Certificate 4");
+    @Override
+    Long getId(Certificate certificate) {
+        return certificate.getId();
     }
 
-    @DisplayName("Should get all leadership experiences")
-    @Test
-    @Order(1)
-    void shouldGetAllLeadershipExperiences() {
-        List<Certificate> all = persistenceService.getAllLeadershipExperiences();
-
-        assertThat(all).hasSize(3);
-        assertThat(all)
-                .extracting(Certificate::getName)
-                .containsExactlyInAnyOrder("LeadershipExperience 1",
-                                           "LeadershipExperience 2",
-                                           "LeadershipExperience 3");
-    }
-
-    @DisplayName("Should create a certificate and a leadership experience")
-    @Transactional
-    @Test
-    @Order(2)
-    void shouldCreate() {
-        Certificate certificate = new Certificate(null,
-                                                  "Created certificate",
-                                                  BigDecimal.valueOf(3),
-                                                  "This is a newly created certificate",
-                                                  Set
-                                                          .of(new Tag(1L, "Important tag"),
-                                                              new Tag(2L, "Way more important tag")));
-
-        Certificate leadershipExperience = new Certificate(null,
-                                                           "Created leadership experience",
-                                                           BigDecimal.valueOf(3),
-                                                           "This is a newly created leadership experience",
-                                                           CertificateType.LEADERSHIP_TRAINING);
-
-        Certificate createdCertificate = persistenceService.create(certificate);
-        Certificate createdLeadershipExperience = persistenceService.create(leadershipExperience);
-
-        assertThat(createdCertificate.getId()).isEqualTo(8L);
-        assertThat(createdCertificate.getName()).isEqualTo(certificate.getName());
-        assertThat(createdCertificate.getPoints()).isEqualByComparingTo(certificate.getPoints());
-        assertThat(createdCertificate.getComment()).isEqualTo(certificate.getComment());
-        assertThat(createdCertificate.getTags()).isEqualTo(certificate.getTags());
-        assertThat(createdCertificate.getCertificateType()).isEqualTo(CertificateType.CERTIFICATE);
-
-        assertThat(createdLeadershipExperience.getId()).isEqualTo(9L);
-        assertThat(createdLeadershipExperience.getName()).isEqualTo(leadershipExperience.getName());
-        assertThat(createdLeadershipExperience.getPoints()).isEqualByComparingTo(leadershipExperience.getPoints());
-        assertThat(createdLeadershipExperience.getComment()).isEqualTo(leadershipExperience.getComment());
-        assertThat(createdLeadershipExperience.getCertificateType()).isEqualTo(CertificateType.LEADERSHIP_TRAINING);
+    @Override
+    void setId(Certificate certificate, Long id) {
+        certificate.setId(id);
     }
 
     @DisplayName("Should update certificate and leadership experience")
@@ -147,11 +73,10 @@ class CertificatePersistenceServiceIT {
                                                            BigDecimal.valueOf(5),
                                                            "This is a updated leadership experience",
                                                            CertificateType.YOUTH_AND_SPORT);
-
-        persistenceService.update(cId, certificate);
-        persistenceService.update(lId, leadershipExperience);
-        Optional<Certificate> certificateResult = persistenceService.getById(cId);
-        Optional<Certificate> leadershipResult = persistenceService.getById(lId);
+        service.update(cId, certificate);
+        service.update(lId, leadershipExperience);
+        Optional<Certificate> certificateResult = service.getById(cId);
+        Optional<Certificate> leadershipResult = service.getById(lId);
 
         assertThat(certificateResult).isPresent();
         Certificate updatedCertificate = certificateResult.get();
@@ -175,16 +100,29 @@ class CertificatePersistenceServiceIT {
         assertThat(leadershipExperience.getCertificateType()).isEqualTo(CertificateType.YOUTH_AND_SPORT);
     }
 
-    @DisplayName("Should delete certificate")
-    @Transactional
+    @DisplayName("Should get all certificates")
     @Test
-    @Order(3)
-    void shouldDelete() {
-        long id = 2;
+    @Order(1)
+    void shouldGetAllCertificates() {
+        List<Certificate> all = service.getAllCertificates();
 
-        persistenceService.delete(id);
+        assertThat(all).hasSize(4);
+        assertThat(all)
+                .extracting(Certificate::getName)
+                .containsExactlyInAnyOrder("Certificate 1", "Certificate 2", "Certificate 3", "Certificate 4");
+    }
 
-        Optional<Certificate> result = persistenceService.getById(id);
-        assertThat(result).isNotPresent();
+    @DisplayName("Should get all leadership experiences")
+    @Test
+    @Order(1)
+    void shouldGetAllLeadershipExperiences() {
+        List<Certificate> all = service.getAllLeadershipExperiences();
+
+        assertThat(all).hasSize(3);
+        assertThat(all)
+                .extracting(Certificate::getName)
+                .containsExactlyInAnyOrder("LeadershipExperience 1",
+                        "LeadershipExperience 2",
+                        "LeadershipExperience 3");
     }
 }
--- backend/src/test/java/ch/puzzle/pcts/service/persistence/ExperienceTypePersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/pcts/service/persistence/ExperienceTypePersistenceServiceIT.java
@@ -1,124 +1,42 @@
 package ch.puzzle.pcts.service.persistence;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import ch.puzzle.pcts.model.experiencetype.ExperienceType;
-import jakarta.transaction.Transactional;
+import ch.puzzle.pcts.repository.ExperienceTypeRepository;
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers
-@ActiveProfiles("test")
-class ExperienceTypePersistenceServiceIT {
+class ExperienceTypePersistenceServiceIT
+        extends
+            PersistenceBaseIT<ExperienceType, ExperienceTypeRepository, ExperienceTypePersistenceService> {
 
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+    ExperienceTypePersistenceServiceIT(ExperienceTypePersistenceService service) {
+        super(service);
     }
 
-    @Autowired
-    private ExperienceTypePersistenceService persistenceService;
-
-    @DisplayName("Should establish DB connection")
-    @Test
-    @Order(0)
-    void shouldEstablishConnection() {
-        assertThat(postgres.isRunning()).isTrue();
+    @Override
+    ExperienceType getCreateEntity() {
+        return new ExperienceType(null,
+                                  "ExperienceType 3",
+                                  BigDecimal.valueOf(10.055),
+                                  BigDecimal.valueOf(5.603),
+                                  BigDecimal.valueOf(2.005));
     }
 
-    @DisplayName("Should get experienceType by id")
-    @Test
-    @Order(1)
-    void shouldGetExperienceTypeById() {
-        Optional<ExperienceType> experienceType = persistenceService.getById(2L);
-
-        assertThat(experienceType).isPresent();
-        assertThat(experienceType.get().getId()).isEqualTo(2L);
+    @Override
+    ExperienceType getUpdateEntity() {
+        return new ExperienceType(null,
+                                  "Updated experienceType",
+                                  BigDecimal.valueOf(10.055),
+                                  BigDecimal.valueOf(5.603),
+                                  BigDecimal.valueOf(2.005));
     }
 
-    @DisplayName("Should get all experienceTypes")
-    @Test
-    @Order(1)
-    void shouldGetAllExperienceTypes() {
-        List<ExperienceType> all = persistenceService.getAll();
-
-        assertThat(all).hasSize(2);
-        assertThat(all)
-                .extracting(ExperienceType::getName)
-                .containsExactlyInAnyOrder("ExperienceType 1", "ExperienceType 2");
+    @Override
+    Long getId(ExperienceType experienceType) {
+        return experienceType.getId();
     }
 
-    @DisplayName("Should create experienceType")
-    @Transactional
-    @Test
-    @Order(2)
-    void shouldCreate() {
-        ExperienceType experienceType = new ExperienceType(null,
-                                                           "ExperienceType 3",
-                                                           BigDecimal.valueOf(10.055),
-                                                           BigDecimal.valueOf(5.603),
-                                                           BigDecimal.valueOf(2.005));
-
-        ExperienceType result = persistenceService.create(experienceType);
-
-        assertThat(result.getId()).isEqualTo(3L);
-        assertThat(result.getName()).isEqualTo(experienceType.getName());
-        assertThat(result.getHighlyRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(10.055));
-        assertThat(result.getLimitedRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(5.603));
-        assertThat(result.getLittleRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(2.005));
-    }
-
-    @DisplayName("Should update experienceType")
-    @Transactional
-    @Test
-    @Order(2)
-    void shouldUpdate() {
-        long id = 2;
-        ExperienceType updated = new ExperienceType(null,
-                                                    "Updated experienceType",
-                                                    BigDecimal.valueOf(10.055),
-                                                    BigDecimal.valueOf(5.603),
-                                                    BigDecimal.valueOf(2.005));
-
-        persistenceService.update(id, updated);
-        Optional<ExperienceType> result = persistenceService.getById(id);
-
-        assertThat(result).isPresent();
-        assertThat(result.get().getId()).isEqualTo(id);
-        assertThat(result.get().getName()).isEqualTo("Updated experienceType");
-        assertThat(result.get().getHighlyRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(10.055));
-        assertThat(result.get().getLimitedRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(5.603));
-        assertThat(result.get().getLittleRelevantPoints()).isEqualByComparingTo(BigDecimal.valueOf(2.005));
-    }
-
-    @DisplayName("Should delete experienceType")
-    @Transactional
-    @Test
-    @Order(3)
-    void shouldDelete() {
-        long id = 2;
-
-        persistenceService.delete(id);
-
-        Optional<ExperienceType> result = persistenceService.getById(id);
-        assertThat(result).isEmpty();
+    @Override
+    void setId(ExperienceType experienceType, Long id) {
+        experienceType.setId(id);
     }
 }
