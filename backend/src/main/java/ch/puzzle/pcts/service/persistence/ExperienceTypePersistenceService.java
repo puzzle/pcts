808e55f12efa167fc43cdc916cb2f01870c829b0
Livio Canobbio <canobbio@puzzle.ch>
2025-10-08 15:20:57 +0200

    feat: implement persistence base to the other services #109

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/ExperienceTypePersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/ExperienceTypePersistenceService.java
@@ -2,42 +2,15 @@ package ch.puzzle.pcts.service.persistence;
 
 import ch.puzzle.pcts.model.experiencetype.ExperienceType;
 import ch.puzzle.pcts.repository.ExperienceTypeRepository;
-import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ExperienceTypePersistenceService {
+public class ExperienceTypePersistenceService extends PersistenceBase<ExperienceType, ExperienceTypeRepository> {
 
     private final ExperienceTypeRepository repository;
 
     public ExperienceTypePersistenceService(ExperienceTypeRepository repository) {
+        super(repository);
         this.repository = repository;
-    }
-
-    @Transactional
-    public ExperienceType create(ExperienceType experienceType) {
-        experienceType.setName(experienceType.getName().trim());
-        return repository.save(experienceType);
-    }
-
-    public Optional<ExperienceType> getById(long id) {
-        return repository.findById(id);
-    }
-
-    public List<ExperienceType> getAll() {
-        return repository.findAll();
-    }
-
-    @Transactional
-    public ExperienceType update(Long id, ExperienceType experienceType) {
-        experienceType.setName(experienceType.getName().trim());
-        experienceType.setId(id);
-        return repository.save(experienceType);
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
     }
 }
