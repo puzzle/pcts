7bb3907c6846a911053b8280f290e05efc44f73a
Livio Canobbio <canobbio@puzzle.ch>
2025-10-09 10:57:30 +0200

    chore: change component annotation to service and deleted autowired over constructor #109

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/OrganisationUnitPersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/OrganisationUnitPersistenceService.java
@@ -3,14 +3,12 @@ package ch.puzzle.pcts.service.persistence;
 import ch.puzzle.pcts.model.organisationunit.OrganisationUnit;
 import ch.puzzle.pcts.repository.OrganisationUnitRepository;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class OrganisationUnitPersistenceService extends PersistenceBase<OrganisationUnit, OrganisationUnitRepository> {
     private final OrganisationUnitRepository repository;
 
-    @Autowired
     public OrganisationUnitPersistenceService(OrganisationUnitRepository repository) {
         super(repository);
         this.repository = repository;
