cf1972125d47bae6d2a9ec3de76e983861b7f777
Livio Canobbio <canobbio@puzzle.ch>
2025-10-10 11:28:20 +0200

    feat: check if fild is null and removed unused instance #111

--- backend/src/main/java/ch/puzzle/pcts/service/validation/ValidationBase.java
+++ backend/src/main/java/ch/puzzle/pcts/service/validation/ValidationBase.java
@@ -2,30 +2,22 @@ package ch.puzzle.pcts.service.validation;
 
 import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.model.error.ErrorKey;
-import ch.puzzle.pcts.service.persistence.RolePersistenceService;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 
 /**
  * @param <T>
  *            the Type or entity of the repository
- * @param <R>
- *            the Repository of the entity
- * @param <P>
- *            the Persistence Service of this repository and entity
  */
-public abstract class ValidationBase<T, R extends JpaRepository<T, Long>, P extends RolePersistenceService> {
+public abstract class ValidationBase<T> {
     private final Validator validator;
-    private final P persistenceService;
 
-    ValidationBase(P persistenceService) {
-        this.persistenceService = persistenceService;
+    ValidationBase() {
         try (ValidatorFactory factory = Validation
                 .byDefaultProvider()
                 .configure()
