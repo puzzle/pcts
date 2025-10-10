042cc88b206530855b6ea671f31e7d7e5331d427
Livio Canobbio <canobbio@puzzle.ch>
2025-10-10 11:10:52 +0200

    chore: removed all example components in the frontend #155

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './features/example/example.component';
 import { MemberOverviewComponent } from './features/member/overview/member-overview.component';
 import { MemberOverviewResolver } from './features/member/overview/member-overview-resolver';
 
 export const routes: Routes = [{
-  path: 'example',
-  component: ExampleComponent
-},
-{
   path: '',
   component: MemberOverviewComponent,
   resolve: { filters: MemberOverviewResolver }
