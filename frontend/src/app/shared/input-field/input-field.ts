da2edf67d0ad5e4844a1af2d675c6ab79aef8449
schiltpzzle <schilt@puzzle.ch>
2025-10-13 10:36:54 +0200

    test: add e2e tests #53

--- frontend/src/app/shared/input-field/input-field.ts
+++ frontend/src/app/shared/input-field/input-field.ts
@@ -1,12 +1,13 @@
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { InputTypeEnum } from './input-type.enum';
-import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
+import {MatDatepickerModule, MatDatepickerToggle} from '@angular/material/datepicker';
+import {InputTypeEnum} from './input-type.enum';
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from '@angular/material/autocomplete';
 
-import { Component, Input, signal, Signal } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { TranslatePipe } from '@ngx-translate/core';
-import { MatError, MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
+import {Component, Input, OnInit, signal, Signal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatError, MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'; // <-- Import this module
 
 @Component({
   selector: 'app-input-field',
@@ -21,30 +22,44 @@ import { MatInput } from '@angular/material/input';
     MatSuffix,
     MatAutocompleteTrigger,
     MatAutocomplete,
-    MatOption
+    MatOption,
+    MatIconModule,
+    MatDatepickerToggle
   ],
   templateUrl: './input-field.html',
   styleUrl: './input-field.scss',
   standalone: true
 })
-export class InputField<T> {
+export class InputField<T> implements OnInit {
   protected readonly InputTypeEnum = InputTypeEnum;
 
   @Input({ required: true }) labelKey!: string;
 
-  @Input({ required: true }) formControl!: FormControl;
+  @Input({required: true}) formGroup!: FormGroup;
+
+  @Input({required: true}) formControlPath!: string;
 
   @Input() type: InputTypeEnum = InputTypeEnum.TEXT;
 
-  @Input() maxErrors = 1;
+  @Input() maxErrors = Number.MAX_SAFE_INTEGER;
 
   @Input() dropdownOptions: Signal<T[]> = signal([]);
 
   @Input() displayWith: (option: T) => string = (option) => String(option);
 
+  get formControl(): FormControl {
+    return this.formGroup.get(this.formControlPath) as FormControl;
+  }
+
+  ngOnInit(): void {
+    if (this.type === InputTypeEnum.DROPDOWN && this.dropdownOptions.length === 0) {
+      this.formControl.disable();
+    }
+  }
+
   getErrorMessages(): string[] {
     const control = this.formControl;
-    if (!control || !control.touched || !control.valid) {
+    if (!control || !control.touched || control.valid) {
       return [];
     }
 
