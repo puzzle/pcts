d119a164e4a9a03bcc2fde458388c34813de1827
Giannin <puzzle@giannin.ch>
2025-10-14 23:22:01 +0200

    test: add architecture tests

--- backend/src/test/java/ch/puzzle/pcts/architecture/CustomConditions.java
+++ backend/src/test/java/ch/puzzle/pcts/architecture/CustomConditions.java
@@ -1,12 +1,41 @@
 package ch.puzzle.pcts.architecture;
 
 import com.tngtech.archunit.core.domain.*;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
 import org.apache.commons.lang3.StringUtils;
+import java.util.Optional;
 
 public class CustomConditions {
+
+    public static final ArchCondition<JavaClass> overrideEqualsMethod = createMethodOverrideCondition("equals",
+            Object.class);
+
+    public static final ArchCondition<JavaClass> overrideToStringMethod = createMethodOverrideCondition("toString");
+
+    public static final ArchCondition<JavaClass> overrideHashCodeMethod = createMethodOverrideCondition("hashCode");
+
+    private static ArchCondition<JavaClass> createMethodOverrideCondition(String methodName, Class<?>... parameters) {
+        return new ArchCondition<>("override %s()".formatted(methodName)) {
+            @Override
+            public void check(JavaClass javaClass, ConditionEvents events) {
+                Optional<JavaMethod> method = javaClass.tryGetMethod(methodName, parameters);
+                boolean isOverridden = method.isPresent() && !method.get().getOwner().isEquivalentTo(Object.class);
+
+                if (!isOverridden) {
+                    String message = String
+                            .format("Class %s in %s does not override %s()",
+                                    javaClass.getName(),
+                                    javaClass.getSourceCodeLocation(),
+                                    methodName);
+                    events.add(SimpleConditionEvent.violated(javaClass, message));
+                }
+            }
+        };
+    };
 
     static ArchCondition<JavaCodeUnit> trimAssignedStringFields() {
         return new ArchCondition<>("use StringUtils.trim() before assigning to String fields") {
@@ -14,11 +43,14 @@ public class CustomConditions {
             public void check(JavaCodeUnit codeUnit, ConditionEvents events) {
                 // Track all fields being set and only take the set accesses
                 for (JavaFieldAccess fieldAccess : codeUnit.getFieldAccesses()) {
-                    if (fieldAccess.getAccessType() != JavaFieldAccess.AccessType.SET)
+                    if (fieldAccess.getAccessType() != JavaFieldAccess.AccessType.SET) {
                         continue;
+                    }
+
                     AccessTarget.FieldAccessTarget targetField = fieldAccess.getTarget();
-                    if (!targetField.getRawType().isEquivalentTo(String.class))
+                    if (!targetField.getRawType().isEquivalentTo(String.class)) {
                         continue;
+                    }
 
                     boolean trimsValue = codeUnit
                             .getMethodCallsFromSelf()
