34a1204f6a3900d0e7fecfb849bf169b5444b7d2
Manuel <moeri@puzzle.ch>
2025-10-28 07:27:33 +0100

    test(backend): Remove public modifier  #144

--- backend/src/test/java/ch/puzzle/pcts/validation/PointsValidationTest.java
+++ backend/src/test/java/ch/puzzle/pcts/validation/PointsValidationTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class PointsValidationTest {
+class PointsValidationTest {
     DummyValidationService service = new DummyValidationService();
 
     @DisplayName("Should throw error when points are null")
