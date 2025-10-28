3b6bd8c3cbadd1248c8b808fe8bee39149399a6b
Miguel Lehmann <lehmann@puzzle.ch>
2025-10-28 10:30:44 +0100

    fix: sonar issues and missing i18n labels #53

--- frontend/src/app/features/organisation-unit/organisation-unit.service.ts
+++ frontend/src/app/features/organisation-unit/organisation-unit.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class OrganisationUnitService {
-  private httpClient = inject(HttpClient);
+  private readonly httpClient = inject(HttpClient);
 
   private readonly API_URL = '/api/v1/organisation-units';
 
