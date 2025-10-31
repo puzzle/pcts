7034f36f0361e22ae83b88cc7ba0d4997647cbaf
Livio Canobbio <canobbio@puzzle.ch>
2025-10-31 14:13:32 +0100

    test: use parameterized test to test some validations #111

--- backend/src/test/java/ch/puzzle/pcts/service/validation/TagValidationServiceTest.java
+++ backend/src/test/java/ch/puzzle/pcts/service/validation/TagValidationServiceTest.java
@@ -21,7 +21,7 @@ class TagValidationServiceTest extends ValidationBaseServiceTest<Tag, TagValidat
     TagPersistenceService persistenceService;
 
     @InjectMocks
-    TagValidationService validationService;
+    TagValidationService service;
 
     @Override
     Tag getValidModel() {
