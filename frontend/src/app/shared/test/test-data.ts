e4172db5884e7c08d1ceb941f4b0b194ebabf181
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-11-03 11:15:51 +0100

    test: make test data use correct date format #46

--- frontend/src/app/shared/test/test-data.ts
+++ frontend/src/app/shared/test/test-data.ts
@@ -28,10 +28,10 @@ export const member1: MemberModel = {
   id: 1,
   firstName: 'Lena',
   lastName: 'Müller',
-  birthDate: DateTime.utc(2000, 12, 1),
+  birthDate: DateTime.fromISO('2000-12-01'),
   abbreviation: 'LM',
   employmentState: EmploymentState.MEMBER,
-  dateOfHire: DateTime.utc(2018, 12, 1),
+  dateOfHire: DateTime.fromISO('2018-12-01'),
   organisationUnit: organisationUnit1
 };
 
@@ -39,10 +39,10 @@ export const member2: MemberModel = {
   id: 2,
   firstName: 'Sophie',
   lastName: 'Keller',
-  birthDate: DateTime.utc(2000, 12, 1),
+  birthDate: DateTime.fromISO('2000-12-01'),
   abbreviation: 'SK',
   employmentState: EmploymentState.MEMBER,
-  dateOfHire: DateTime.utc(2018, 12, 1),
+  dateOfHire: DateTime.fromISO('2018-12-01'),
   organisationUnit: organisationUnit2
 };
 
@@ -50,10 +50,10 @@ export const member3: MemberModel = {
   id: 3,
   firstName: 'Mara',
   lastName: 'Becker',
-  birthDate: DateTime.utc(2000, 12, 1),
+  birthDate: DateTime.fromISO('2000-12-01'),
   abbreviation: 'BD',
   employmentState: EmploymentState.EX_MEMBER,
-  dateOfHire: DateTime.utc(2018, 12, 1),
+  dateOfHire: DateTime.fromISO('2018-12-01'),
   organisationUnit: organisationUnit3
 };
 
@@ -61,21 +61,21 @@ export const member4: MemberModel = {
   id: 4,
   firstName: 'John',
   lastName: 'Doe',
-  birthDate: DateTime.utc(2000, 12, 1),
+  birthDate: DateTime.fromISO('2000-12-01'),
   abbreviation: 'JD',
   employmentState: EmploymentState.APPLICANT,
-  dateOfHire: DateTime.utc(2018, 12, 1),
+  dateOfHire: DateTime.fromISO('2018-12-01'),
   organisationUnit: organisationUnit4
 };
 
 export const memberDto1: MemberDto = {
   firstName: 'Lena',
   lastName: 'Müller',
-  birthDate: DateTime.utc(2000, 12, 1)
+  birthDate: DateTime.fromISO('2018-12-01')
     .toJSDate(),
   abbreviation: 'LM',
   employmentState: EmploymentState.MEMBER,
-  dateOfHire: DateTime.utc(2018, 12, 1)
+  dateOfHire: DateTime.fromISO('2018-12-01')
     .toJSDate(),
   organisationUnitId: 1
 };
