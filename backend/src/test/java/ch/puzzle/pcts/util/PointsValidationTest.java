bbdd941cffb239a4da08769ceaf4dccab614b521
Manuel <moeri@puzzle.ch>
2025-11-03 08:56:21 +0100

    test(util): Refactor location of custom annotation tests to match src  #111

--- backend/src/test/java/ch/puzzle/pcts/util/PointsValidationTest.java
+++ backend/src/test/java/ch/puzzle/pcts/util/PointsValidationTest.java
@@ -1,4 +1,4 @@
-package ch.puzzle.pcts.validation;
+package ch.puzzle.pcts.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.model.Model;
 import ch.puzzle.pcts.service.validation.ValidationBase;
-import ch.puzzle.pcts.util.PointsValidation;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
