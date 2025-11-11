7162332d3bfd0e5689204cda56389bff1e86b6de
Yanick Minder <minder@puzzle.ch>
2025-11-11 10:06:59 +0100

    feat(frontend): refactor GenCol and fix sorting accessor

--- frontend/src/app/shared/generic-table/generic-table.component.ts
+++ frontend/src/app/shared/generic-table/generic-table.component.ts
@@ -15,7 +15,7 @@ import {
 import { MatSort, MatSortHeader } from '@angular/material/sort';
 import { ScopedTranslationPipe } from '../pipes/scoped-translation-pipe';
 import { CaseFormatter } from '../format/case-formatter';
-import { GenericTableDataSource, ProcessedTableColumn } from './GenericTableDataSource';
+import { GenCol, GenericTableDataSource } from './GenericTableDataSource';
 
 
 @Component({
@@ -48,27 +48,43 @@ export class GenericTableComponent<T extends object> implements AfterViewChecked
 
   entries = signal<T[]>([]);
 
-  columns = computed(() => this.dataSource().processedColumns);
+  columns = computed(() => this.dataSource().columnDefs);
 
-  columnNames = computed(() => this.dataSource().processedColumns.map((e) => e.columnName));
+  columnNames = computed(() => this.dataSource().columnDefs.map((e) => e.columnName));
 
   sort = viewChild(MatSort);
 
   constructor() {
     effect(() => {
+      this.dataSource().sortingDataAccessor = (data: T, sortHeaderId: string) => {
+        const find = this.columns()
+          .find((e) => e.columnName == sortHeaderId);
+        if (!find) {
+          return (data[sortHeaderId as keyof T] + '').toLowerCase();
+        }
+
+        const date = new Date(find.getValue(data));
+        const isDate = !isNaN(date?.getTime());
+        if (isDate) {
+          return date.getTime();
+        }
+        return this.getDisplayValue(find, data)
+          .toLowerCase();
+      };
       this.dataSource().sort = this.sort();
     });
   }
+
 
   ngAfterViewChecked(): void {
     this.entries.set(this.dataSource().data);
   }
 
-  getFieldI18nKey(col: ProcessedTableColumn<T>) {
+  getFieldI18nKey(col: GenCol<T>) {
     return this.caseFormatter.camelToSnake(col.columnName);
   }
 
-  protected getDisplayValue(col: ProcessedTableColumn<T>, entity: T) {
+  protected getDisplayValue(col: GenCol<T>, entity: T): string {
     let value = col.getValue(entity) ?? '';
     col.pipes.forEach((formatter) => {
       value = formatter(value);
