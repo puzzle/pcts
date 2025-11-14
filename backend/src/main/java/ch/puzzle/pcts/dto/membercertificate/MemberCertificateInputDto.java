057e1f3e89aa0461e951ebc013cdf47f4e366fc2
Livio Canobbio <canobbio@puzzle.ch>
2025-11-14 10:29:52 +0100

    chore(dto): add swagger docs #48

--- backend/src/main/java/ch/puzzle/pcts/dto/membercertificate/MemberCertificateInputDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/membercertificate/MemberCertificateInputDto.java
@@ -1,8 +1,16 @@
 package ch.puzzle.pcts.dto.membercertificate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
-public record MemberCertificateInputDto(Long memberId, Long certificate_typeId, LocalDate completed_at,
-        LocalDate valid_until, String comment) {
+public record MemberCertificateInputDto(
+        @Schema(description = "The member id that associated with this certificate.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false) Long memberId,
 
+        @Schema(description = "The type of certificate awarded to the member.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false) Long certificate_typeId,
+
+        @Schema(description = "The date when the member completed the certificate.", example = "2025-09-24", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false) LocalDate completed_at,
+
+        @Schema(description = "The date until which the certificate is valid.", example = "2028-02-12", requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true) LocalDate valid_until,
+
+        @Schema(description = "An optional comment for the member certificate.", example = "Completed via fast-track program", requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true) String comment) {
 }
