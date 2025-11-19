7c71d5fed1ae66e09b6ab429e5160a411450cfe3
Manuel <moeri@puzzle.ch>
2025-11-19 11:28:47 +0100

    test(service): Remove unwanted test  #177

--- backend/src/test/java/ch/puzzle/pcts/service/validation/MemberValidationServiceTest.java
+++ backend/src/test/java/ch/puzzle/pcts/service/validation/MemberValidationServiceTest.java
@@ -103,19 +103,14 @@ class MemberValidationServiceTest extends ValidationBaseServiceTest<Member, Memb
                             .of(createMember(null, validPastDate, "Member", "Test", "MT"),
                                 "Member.employmentState must not be null."),
                     Arguments
-                            .of(createMember(EmploymentState.MEMBER, validPastDate, "Member", "Test", tooLongString),
-                                String
-                                        .format("Member.abbreviation size must be between 2 and 250, given %s.",
-                                                tooLongString),
-                                Arguments
-                                        .of(createMember(EmploymentState.MEMBER, null, "Member", "Test", "MT"),
-                                            "Member.birthDate must not be null."),
-                                Arguments
-                                        .of(createMember(EmploymentState.MEMBER, futureDate, "Member", "Test", "MT"),
-                                            "Member.birthDate must be in the past, given " + futureDate + "."),
-                                Arguments
-                                        .of(createMember(EmploymentState.MEMBER, futureDate, "Member", "Test", "MT"),
-                                            "Member.birthDate must be in the past, given " + futureDate + ".")));
+                            .of(createMember(EmploymentState.MEMBER, null, "Member", "Test", "MT"),
+                                "Member.birthDate must not be null."),
+                    Arguments
+                            .of(createMember(EmploymentState.MEMBER, futureDate, "Member", "Test", "MT"),
+                                "Member.birthDate must be in the past, given " + futureDate + "."),
+                    Arguments
+                            .of(createMember(EmploymentState.MEMBER, futureDate, "Member", "Test", "MT"),
+                                "Member.birthDate must be in the past, given " + futureDate + "."));
     }
 
     @DisplayName("Should call correct validate method on validateOnCreate()")
