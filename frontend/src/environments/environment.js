663c8afc89e113c76a079e1a24b454c165a6b976
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-11-21 13:44:49 +0100

    feat: make proxy conf a js to allow generic targets depending on env

--- frontend/src/environments/environment.js
+++ frontend/src/environments/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  staging: false
+  apiUrl: 'http://localhost:8080'
 };
---
+++ frontend/src/proxy.conf.js
@@ -0,0 +1,10 @@
+const { environment } = require('./environments/environment')
+
+const target = environment.apiUrl || "http://localhost:8080";
+
+module.exports = {
+  "/api": {
+    target,
+    secure: false,
+  }
+};
