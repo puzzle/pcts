b8e1cf61b34e261bf070df109e057ef9e581df54
Giannin <puzzle@giannin.ch>
2025-11-26 11:37:21 +0100

    feat: wip set up oauth for frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,7 +7,7 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import localeDeCH from '@angular/common/locales/de-CH';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTranslateService, TranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
@@ -17,6 +17,8 @@ import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { lastValueFrom } from 'rxjs';
 import { registerLocaleData } from '@angular/common';
 import { provideI18nPrefix } from './shared/i18n-prefix.provider';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { oAuthInterceptor } from './shared/interceptors/o-auth-interceptor';
 
 registerLocaleData(localeDeCH);
 export const appConfig: ApplicationConfig = {
@@ -24,7 +26,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([oAuthInterceptor])),
+    provideOAuthClient(),
     provideTranslateService({
       fallbackLang: 'de',
       loader: provideTranslateHttpLoader({
