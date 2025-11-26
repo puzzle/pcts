d7172ff0c0740c32c0771a6753d96d739a79347b
Giannin <puzzle@giannin.ch>
2025-11-26 11:37:21 +0100

    feat: wip set up oauth for frontend
    # Conflicts:
    #	frontend/src/app/app.routes.ts

--- frontend/src/environments/environment.js
+++ frontend/src/environments/environment.js
@@ -1,4 +1,16 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
+  oauth: {
+    decreaseExpirationBySec: 30,
+    clearHashAfterLogin: true,
+    issuer: 'http://localhost:8544',
+    strictDiscoveryDocumentValidation: false,
+    redirectUri: `http://localhost:8544`, //TODO: make this dynamic again
+    requireHttps: false,
+    scope: 'openid profile',
+    clientId: '',
+    responseType: 'code',
+    showDebugInformation: true
+  }
 }
