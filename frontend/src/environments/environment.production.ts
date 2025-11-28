002888e60e31b1f7297247845ea38b4a2c4e6250
Giannin <puzzle@giannin.ch>
2025-11-28 15:20:27 +0100

    feat: wip debugging

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://pcts-backend:8080',
+  oauth: {
+    decreaseExpirationBySec: 30,
+    clearHashAfterLogin: true,
+    issuer: 'https://sso.puzzle.ch/auth/realms/pitc',
+    strictDiscoveryDocumentValidation: false,
+    redirectUri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}`,
+    scope: 'profile openid',
+    clientId: 'pitc_pcts_prod',
+    responseType: 'code',
+    showDebugInformation: false
+  }
+};
