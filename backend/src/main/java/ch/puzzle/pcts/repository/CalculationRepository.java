e5b5f021e8d3e4865332418bac37401dd6a3649f
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-11-28 11:17:02 +0100

    fix: add missing spring annotations #41

--- backend/src/main/java/ch/puzzle/pcts/repository/CalculationRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/CalculationRepository.java
@@ -2,6 +2,8 @@ package ch.puzzle.pcts.repository;
 
 import ch.puzzle.pcts.model.calculation.Calculation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CalculationRepository extends JpaRepository<Calculation, Long> {
 }
