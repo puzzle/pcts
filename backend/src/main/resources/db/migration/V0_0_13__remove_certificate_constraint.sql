c8dcd89f0e07b65720ae815fcfae225cc8c8bcbf
Manuel <moeri@puzzle.ch>
2025-12-03 07:53:51 +0100

    fix(db): Create seperate migration for removing the constraint  #49

---
+++ backend/src/main/resources/db/migration/V0_0_13__remove_certificate_constraint.sql
@@ -0,0 +1,2 @@
+ALTER TABLE certificate
+    ALTER COLUMN completed_at DROP NOT NULL;
--- backend/src/main/resources/db/migration/V0_0_9__certificate.sql
+++ backend/src/main/resources/db/migration/V0_0_9__certificate.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS certificate
     id                      BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     member_id               BIGINT NOT NULL,
     certificate_type_id     BIGINT NOT NULL,
-    completed_at            DATE,
+    completed_at            DATE NOT NULL,
     valid_until             DATE,
     comment                 TEXT,
     deleted_at              TIMESTAMP DEFAULT NULL
