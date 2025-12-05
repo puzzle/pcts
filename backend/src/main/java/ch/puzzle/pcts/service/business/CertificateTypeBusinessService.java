91eb09f3fa96bb0a85e1cffbccb6b389d36cc494
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-12-05 13:12:56 +0100

    refactor: format code #289

--- backend/src/main/java/ch/puzzle/pcts/service/business/CertificateTypeBusinessService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/business/CertificateTypeBusinessService.java
@@ -1,22 +1,19 @@
 package ch.puzzle.pcts.service.business;
 
+import static ch.puzzle.pcts.Constants.CERTIFICATE_TYPE;
+
 import ch.puzzle.pcts.dto.error.ErrorKey;
 import ch.puzzle.pcts.dto.error.FieldKey;
 import ch.puzzle.pcts.dto.error.GenericErrorDto;
 import ch.puzzle.pcts.exception.PCTSException;
-import ch.puzzle.pcts.model.certificate.Certificate;
 import ch.puzzle.pcts.model.certificatetype.CertificateKind;
 import ch.puzzle.pcts.model.certificatetype.CertificateType;
 import ch.puzzle.pcts.service.persistence.CertificateTypePersistenceService;
 import ch.puzzle.pcts.service.validation.CertificateTypeValidationService;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
-import static ch.puzzle.pcts.Constants.CERTIFICATE_TYPE;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CertificateTypeBusinessService extends BusinessBase<CertificateType> {
@@ -34,7 +31,7 @@ public class CertificateTypeBusinessService extends BusinessBase<CertificateType
     }
 
     @Override
-    public CertificateType getById(Long id){
+    public CertificateType getById(Long id) {
         CertificateType certificateType = super.getById(id);
         validateCertificateIsPresent(certificateType);
         return certificateType;
@@ -70,9 +67,14 @@ public class CertificateTypeBusinessService extends BusinessBase<CertificateType
     }
 
     private void validateCertificateIsPresent(CertificateType certificateType) {
-        if (certificateType.getCertificateKind() != CertificateKind.CERTIFICATE){
+        if (certificateType.getCertificateKind() != CertificateKind.CERTIFICATE) {
             Map<FieldKey, String> attributes = Map
-                    .of(FieldKey.ENTITY, entityName(), FieldKey.FIELD, "id", FieldKey.IS, certificateType.getId().toString());
+                    .of(FieldKey.ENTITY,
+                        entityName(),
+                        FieldKey.FIELD,
+                        "id",
+                        FieldKey.IS,
+                        certificateType.getId().toString());
 
             GenericErrorDto error = new GenericErrorDto(ErrorKey.NOT_FOUND, attributes);
             throw new PCTSException(HttpStatus.NOT_FOUND, List.of(error));
