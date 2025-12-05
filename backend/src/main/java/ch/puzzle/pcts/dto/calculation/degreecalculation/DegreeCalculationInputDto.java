f9fb81a3b7df0fc077c903684cae20917511e842
Miguel Lehmann <lehmann@puzzle.ch>
2025-12-05 11:45:40 +0100

    feat: add mapper for calculation #42

--- backend/src/main/java/ch/puzzle/pcts/dto/calculation/degreecalculation/DegreeCalculationInputDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/calculation/degreecalculation/DegreeCalculationInputDto.java
@@ -2,7 +2,8 @@ package ch.puzzle.pcts.dto.calculation.degreecalculation;
 
 import ch.puzzle.pcts.model.calculation.Relevancy;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
 
-public record DegreeCalculationInputDto(@Schema(example = "1") Long id, @Schema(example = "1") int weight,
+public record DegreeCalculationInputDto(@Schema(example = "1") Long degreeId, @Schema(example = "1") BigDecimal weight,
         @Schema(example = "highly") Relevancy relevancy) {
 }
