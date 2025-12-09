6ccdf7f272341deefe13defc03b9e6cfd95f4984
Miguel Lehmann <lehmann@puzzle.ch>
2025-12-09 16:12:44 +0100

    fix: move and fix points logic #42

--- backend/src/main/java/ch/puzzle/pcts/repository/ExperienceCalculationRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/ExperienceCalculationRepository.java
@@ -1,7 +1,6 @@
 package ch.puzzle.pcts.repository;
 
 import ch.puzzle.pcts.model.calculation.experiencecalculation.ExperienceCalculation;
-import ch.puzzle.pcts.model.experience.Experience;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExperienceCalculationRepository extends JpaRepository<ExperienceCalculation, Long> {
     List<ExperienceCalculation> findByCalculationId(Long calculationId);
-    List<ExperienceCalculation> findByExperience(Experience experience);
+    List<ExperienceCalculation> findByExperienceId(Long experienceId);
 }
