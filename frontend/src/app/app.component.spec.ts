543708e2ad274901c6e5a731ef2b1f66961bdc95
Giannin <puzzle@giannin.ch>
2025-12-17 16:19:57 +0100

    test: fix unit test issues for new auth features

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,14 +2,26 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
+import { UserService } from './core/auth/user.service';
+
 jest.mock('@puzzleitc/puzzle-shell', () => jest.fn());
 
 describe('AppComponent', () => {
   beforeEach(async() => {
+    const userServiceMock = {
+      getName: jest.fn()
+        .mockReturnValue('Test User'),
+      logout: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [provideTranslateService(),
-        provideRouter([])]
+        provideRouter([]),
+        {
+          provide: UserService,
+          useValue: userServiceMock
+        }]
     })
       .compileComponents();
   });
