f1131232a9613d4eae32655b83113c2e562358b9
Manuel <moeri@puzzle.ch>
2025-12-17 16:12:57 +0100

    fix(scoped-translation-service): Remove any as provider and simply provide the service in the I18nPrefix function  #336

--- frontend/src/app/shared/i18n-prefix.provider.ts
+++ frontend/src/app/shared/i18n-prefix.provider.ts
@@ -1,8 +1,9 @@
 import { Optional, Provider, SkipSelf } from '@angular/core';
 import { I18N_PREFIX } from './i18n-prefix.token';
+import { ScopedTranslationService } from './services/scoped-translation.service';
 
-export function provideI18nPrefix(suffix: string, separator = '.'): Provider {
-  return {
+export function provideI18nPrefix(suffix: string, separator = '.'): Provider[] {
+  return [{
     provide: I18N_PREFIX,
     useFactory: (parentPrefix: string | null): string => {
       if (!suffix) {
@@ -15,5 +16,6 @@ export function provideI18nPrefix(suffix: string, separator = '.'): Provider {
     deps: [[new SkipSelf(),
       new Optional(),
       I18N_PREFIX]]
-  };
+  },
+  ScopedTranslationService];
 }
