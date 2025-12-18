dd0015426189eec3da8c9a8d27b1650ce29f0609
Giannin <contact@giannin.ch>
2025-12-18 22:36:19 +0100

    feat(auth): make keycloak parameters configurable, fix docker compose

--- frontend/src/proxy.conf.js
+++ frontend/src/proxy.conf.js
@@ -1,10 +1,9 @@
-const { environment } = require('./environments/environment')
-
-const target = environment.apiUrl || 'http://localhost:8080'
+const target = 'http://localhost:8080';
 
 module.exports = {
   '/api': {
     target,
     secure: false,
+    changeOrigin: true
   },
 }
