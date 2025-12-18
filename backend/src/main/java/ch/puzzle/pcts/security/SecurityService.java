ef19ce33138453a5d56736883c03ef224952f49b
Giannin <contact@giannin.ch>
2025-12-18 16:47:18 +0100

    feat(auth): partially implement filter to allow access to users own resources #44

--- backend/src/main/java/ch/puzzle/pcts/security/SecurityService.java
+++ backend/src/main/java/ch/puzzle/pcts/security/SecurityService.java
@@ -1,7 +1,11 @@
 package ch.puzzle.pcts.security;
 
+import ch.puzzle.pcts.model.member.Member;
+import ch.puzzle.pcts.service.business.MemberBusinessService;
+import ch.puzzle.pcts.util.AuthenticatedUserHelper;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,8 +13,14 @@ import org.springframework.stereotype.Component;
 
 @Component("SecurityService")
 public class SecurityService {
+    private final MemberBusinessService memberService;
+
     @Value("#{'${pcts.authentication.admin-authorities}'.split(',')}")
     private List<String> adminAuthorities;
+
+    public SecurityService(MemberBusinessService memberService) {
+        this.memberService = memberService;
+    }
 
     public boolean isAdmin() {
         return getAuthorities()
@@ -19,8 +29,17 @@ public class SecurityService {
                 .anyMatch(getAdminAuthoritiesAsRoles()::contains);
     }
 
-    public boolean isUsersOwnSheet() {
-        // TODO
+    public boolean isUsersOwnSheet(Long userId) {
+        Optional<Member> member = memberService.findIfExists(userId);
+        if (member.isEmpty()) {
+            return false;
+        }
+
+        Optional<String> email = AuthenticatedUserHelper.getEmail();
+        if (email.isEmpty()) {
+            return false;
+        }
+
         return false;
     }
 
