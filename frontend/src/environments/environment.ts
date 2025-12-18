e5b121419ac170c9313b7348fe06989d90cbe7b8
Giannin <contact@giannin.ch>
2025-12-18 22:36:19 +0100

    feat(auth): make keycloak parameters configurable, fix docker compose

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  keycloak: {
+    url: 'http://localhost:8544',
+    realm: 'pitc',
+    clientId: 'pcts-dev'
+  }
+};
