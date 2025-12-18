d06f4a6d1feaceb44b3418193189fca4037f15c0
Linus Ammon <ammon@puzzle.ch>
2025-12-18 08:36:57 +0100

    fix(test): update webmvc related imports

--- backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
+++ backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
@@ -7,7 +7,7 @@ import ch.puzzle.pcts.util.IT;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
