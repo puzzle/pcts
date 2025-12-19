d77435595ea848d972b8eff101d035d19d34663f
Manuel <moeri@puzzle.ch>
2025-12-19 15:18:18 +0100

    refactor(dto): Add overview dtos for only the needed attributes  #50

--- backend/src/main/resources/db/migration/V0_0_14__add_member_view.sql
+++ backend/src/main/resources/db/migration/V0_0_14__add_member_view.sql
@@ -1,54 +1,56 @@
 DROP VIEW IF EXISTS member_overview;
 
 CREATE VIEW member_overview AS
-SELECT
-    m.id                   AS member_id,
-    m.first_name           AS member_first_name,
-    m.last_name            AS member_last_name,
-    m.abbreviation         AS member_abbreviation,
-    m.employment_state     AS member_employment_state,
-    m.date_of_hire         AS member_date_of_hire,
-    m.birth_date           AS member_birth_date,
-    ou.name                AS organisation_unit_name,
+SELECT m.id                AS member_id,
+       m.first_name,
+       m.last_name,
+       m.abbreviation,
+       m.employment_state,
+       m.date_of_hire,
+       m.birth_date,
 
-    c.id                   AS certificate_id,
-    c.completed_at         AS certificate_completed_at,
-    c.valid_until          AS certificate_valid_until,
-    c.comment              AS certificate_comment,
+       ou.name             AS organisation_unit_name,
 
-    ct.name                AS certificate_type_name,
-    ct.comment             AS certificate_type_comment,
+       c.id                AS certificate_id,
+       c.completed_at,
+       c.valid_until,
+       c.comment           AS certificate_comment,
 
-    l.id                   AS leadership_id,
-    l.comment              AS leadership_comment,
+       ct.name             AS certificate_type_name,
+       ct.comment          AS certificate_type_comment,
+       ct.certificate_kind AS certificate_type_kind,
 
-    lt.name                AS leadership_type_name,
-    lt.comment             AS leadership_type_comment,
-    lt.certificate_kind    AS leadership_type_kind,
+       d.id                AS degree_id,
+       d.name              AS degree_name,
+       d.start_date        AS degree_start_date,
+       d.end_date          AS degree_end_date,
+       d.comment           AS degree_comment,
+       dt.name             AS degree_type_name,
 
-    d.id                   AS degree_id,
-    d.name                 AS degree_name,
-    d.start_date           AS degree_start_date,
-    d.end_date             AS degree_end_date,
-
-    dt.name                AS degree_type_name,
-
-    e.id                   AS experience_id,
-    e.name                 AS experience_name,
-    e.employer             AS experience_employer,
-    e.start_date           AS experience_start_date,
-    e.end_date             AS experience_end_date,
-    e.comment              AS experience_comment,
-
-    et.name                AS experience_type_name
+       e.id                AS experience_id,
+       e.name              AS experience_name,
+       e.employer,
+       e.start_date        AS experience_start_date,
+       e.end_date          AS experience_end_date,
+       e.comment           AS experience_comment,
+       et.name             AS experience_type_name
 
 FROM member m
-         LEFT JOIN organisation_unit ou ON ou.id = m.organisation_unit
-         LEFT JOIN certificate c ON c.member_id = m.id
-         LEFT JOIN certificate_type ct ON ct.id = c.certificate_type_id
-         LEFT JOIN certificate l ON l.member_id = m.id
-         LEFT JOIN certificate_type lt ON lt.id = l.certificate_type_id
-         LEFT JOIN degree d ON d.member_id = m.id
-         LEFT JOIN degree_type dt ON dt.id = d.degree_type_id
-         LEFT JOIN experience e ON e.member_id = m.id
-         LEFT JOIN experience_type et ON et.id = e.experience_type_id
+
+         LEFT JOIN organisation_unit ou
+                   ON ou.id = m.organisation_unit
+
+         LEFT JOIN certificate c
+                   ON c.member_id = m.id
+         LEFT JOIN certificate_type ct
+                   ON ct.id = c.certificate_type_id
+
+         LEFT JOIN degree d
+                   ON d.member_id = m.id
+         LEFT JOIN degree_type dt
+                   ON dt.id = d.degree_type_id
+
+         LEFT JOIN experience e
+                   ON e.member_id = m.id
+         LEFT JOIN experience_type et
+                   ON et.id = e.experience_type_id;
