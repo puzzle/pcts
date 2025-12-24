3b82a7e9c9419f2c9445a4fa0fb70582cab63241
Giannin <puzzle@giannin.ch>
2025-12-24 16:21:39 +0100

    docs(auth): add @Documented annotation

--- backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAdminOrOwner.java
+++ backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAdminOrOwner.java
@@ -1,4 +1,5 @@
 package ch.puzzle.pcts.security.annotation;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
  * Combined check: Access allowed if user is an Admin OR the Owner of the
  * resource.
  */
+@Documented
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("@SecurityService.isAdmin() or @SecurityService.isOwner(#id)")
