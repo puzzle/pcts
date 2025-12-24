45cb556aabe4ff8911ad55334f5a55e2547c4d5b
Giannin <puzzle@giannin.ch>
2025-12-24 17:18:40 +0100

    test(auth): add tests for the directive #44

--- frontend/src/app/core/auth/directive/show-if-admin.directive.spec.ts
+++ frontend/src/app/core/auth/directive/show-if-admin.directive.spec.ts
@@ -1,9 +1,54 @@
 import { ShowIfAdminDirective } from './show-if-admin.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserService } from '../user.service';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('ShowIfAdminDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowIfAdminDirective();
-    expect(directive)
+@Component({
+  standalone: true,
+  imports: [ShowIfAdminDirective],
+  template: '<div *appShowIfAdmin id="admin-content">Admin Only View</div>'
+})
+class ShowIfAdminDirectiveSpecComponent {}
+
+describe('ShowIfAdminDirective (Jest)', () => {
+  let fixture: ComponentFixture<ShowIfAdminDirectiveSpecComponent>;
+
+  const mockUserService = {
+    isAdmin: jest.fn()
+  };
+
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [ShowIfAdminDirectiveSpecComponent,
+        ShowIfAdminDirective],
+      providers: [{ provide: UserService,
+        useValue: mockUserService }]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(ShowIfAdminDirectiveSpecComponent);
+  });
+
+  it('should render content if user is admin', () => {
+    mockUserService.isAdmin.mockReturnValue(true);
+
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('#admin-content'));
+    expect(element)
       .toBeTruthy();
+    expect(element.nativeElement.textContent)
+      .toContain('Admin Only View');
+  });
+
+  it('should NOT render content if user is not admin', () => {
+    mockUserService.isAdmin.mockReturnValue(false);
+
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('#admin-content'));
+    expect(element)
+      .toBeFalsy();
   });
 });
