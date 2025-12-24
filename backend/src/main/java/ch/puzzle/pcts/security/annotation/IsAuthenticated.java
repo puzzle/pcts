d535b365b363a9ce850f408fcfa1fd8ba26489b6
Giannin <puzzle@giannin.ch>
2025-12-24 16:21:39 +0100

    docs(auth): add @Documented annotation

--- backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAuthenticated.java
+++ backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAuthenticated.java
@@ -1,11 +1,16 @@
 package ch.puzzle.pcts.security.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+/**
+ * Validates that the user is authenticated but has not further requirements.
+ */
+@Documented
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("isAuthenticated()")
