dc5a50dd458388ef1adc804140d9fcedf4c89284
Giannin <puzzle@giannin.ch>
2025-12-24 16:21:39 +0100

    docs(auth): add @Documented annotation

--- backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAdmin.java
+++ backend/src/main/java/ch/puzzle/pcts/security/annotation/IsAdmin.java
@@ -1,5 +1,6 @@
 package ch.puzzle.pcts.security.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 /**
  * Check whether a user is part of the configured admin groups
  */
+@Documented
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("@SecurityService.isAdmin()")
