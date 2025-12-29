24cb6664c905c2b4cf030f4192f11c07e1905831
Giannin <contact@giannin.ch>
2025-12-29 19:17:49 +0100

    refactor(auth): add users without email to dev data #44

--- backend/src/main/resources/db/dev-data-migration/R__after_migrate.sql
+++ backend/src/main/resources/db/dev-data-migration/R__after_migrate.sql
@@ -96,7 +96,8 @@ VALUES
     ( 'Mara', 'Becker', 'becker@puzzle.ch', 'mbecker', 'MB', 'EX_MEMBER', '2023-01-09', '2001-08-05', null, 5, '2025-01-28', 0),
     ( 'Felix', 'Hofmann', 'hofmann@puzzle.ch', 'fhofmann', 'FH', 'EX_MEMBER', '2021-09-17', '2000-02-29', 1, 6, '2025-01-28', 0),
     ( 'Jaya', 'Norris', 'gl@gl.com','ynorris', 'JN', 'MEMBER', '2021-09-17', '2000-02-29', 1, null, null, null),
-    ( 'Abraham', ' Woodard', 'member@member.com', 'awoodard', 'AW', 'MEMBER', '2021-09-17', '2004-02-29', 3, null, null, null);
+    ( 'Abraham', ' Woodard', 'member@member.com', 'awoodard', 'AW', 'MEMBER', '2021-09-17', '2004-02-29', 3, null, null, null),
+    ('Ashleigh', 'Russell', null, 'arussell', 'AR', 'APPLICANT', null, '1998-02-19', null, null, null, null);
 
 INSERT INTO experience (member_id, name, employer, percent, experience_type_id, comment, start_date, end_date)
 VALUES
