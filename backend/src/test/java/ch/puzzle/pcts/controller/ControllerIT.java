72013d06608060cda09c6233996f44235caea52c
Giannin <contact@giannin.ch>
2025-12-30 17:42:46 +0100

    refactor(auth): fix minor things after rebase #44

--- backend/src/test/java/ch/puzzle/pcts/controller/ControllerIT.java
+++ backend/src/test/java/ch/puzzle/pcts/controller/ControllerIT.java
@@ -7,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
 
