1a0987d3688efd7923ab7b9243602baa73af2b4b
Giannin <contact@giannin.ch>
2025-12-30 20:28:37 +0100

    test(auth): remove unused mocks in test #44

--- backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
+++ backend/src/test/java/ch/puzzle/pcts/ApplicationAvailabilityTest.java
@@ -8,14 +8,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import tools.jackson.databind.json.JsonMapper;
 
 @IT
 @AutoConfigureMockMvc
@@ -26,12 +23,6 @@ class ApplicationAvailabilityTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private JsonMapper jsonMapper;
-
-    @MockitoBean
-    private JwtDecoder jwtDecoder;
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
