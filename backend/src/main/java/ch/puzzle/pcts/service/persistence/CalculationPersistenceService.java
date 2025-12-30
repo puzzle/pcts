8cf40ef88fd0dd359eb13c29ebe859935693a7fe
Giannin <contact@giannin.ch>
2025-12-30 17:05:32 +0100

    refactor(auth): improve UserService.java and rename it to JwtService #44

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/CalculationPersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/CalculationPersistenceService.java
@@ -5,19 +5,19 @@ import static ch.puzzle.pcts.Constants.CALCULATION;
 import ch.puzzle.pcts.model.calculation.Calculation;
 import ch.puzzle.pcts.model.calculation.CalculationState;
 import ch.puzzle.pcts.repository.CalculationRepository;
-import ch.puzzle.pcts.service.UserService;
+import ch.puzzle.pcts.service.JwtService;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculationPersistenceService extends PersistenceBase<Calculation, CalculationRepository> {
-    private final UserService userService;
+    private final JwtService jwtService;
     private final CalculationRepository repository;
 
-    public CalculationPersistenceService(UserService userService, CalculationRepository repository) {
+    public CalculationPersistenceService(JwtService jwtService, CalculationRepository repository) {
         super(repository);
-        this.userService = userService;
+        this.jwtService = jwtService;
         this.repository = repository;
     }
 
@@ -37,7 +37,7 @@ public class CalculationPersistenceService extends PersistenceBase<Calculation, 
 
     private void setPublicationFields(Calculation calculation) {
         calculation.setPublicationDate(LocalDate.now());
-        calculation.setPublicizedBy(this.userService.getDisplayName());
+        calculation.setPublicizedBy(this.jwtService.getDisplayName());
     }
 
     private void setStateOfOldActiveCalculationsToArchived(Calculation calculation) {
