d741c6bce2e2298e84410f36cbf616bca52a5ad6
Giannin <contact@giannin.ch>
2025-12-30 20:34:57 +0100

    chore(auth): run formatter #44

--- backend/src/test/java/ch/puzzle/pcts/service/business/MemberBusinessServiceTest.java
+++ backend/src/test/java/ch/puzzle/pcts/service/business/MemberBusinessServiceTest.java
@@ -27,6 +27,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
