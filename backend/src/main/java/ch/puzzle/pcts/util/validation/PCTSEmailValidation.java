04a0895ca2e7645d7af05afe4183ac7280680f3f
Giannin <contact@giannin.ch>
2025-12-30 15:18:20 +0100

    refactor(auth): combine email validations into a single annotation #44

--- backend/src/main/java/ch/puzzle/pcts/util/validation/PCTSEmailValidation.java
+++ backend/src/main/java/ch/puzzle/pcts/util/validation/PCTSEmailValidation.java
@@ -2,30 +2,37 @@ package ch.puzzle.pcts.util.validation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * <h3>Validation rules:</h3>
  * <ul>
  * <li>The value can be {@code null}.</li>
- * <li>If the value is not {@code null}, it must contain at least one
- * non-whitespace character</li>
+ * <li>If the value is not {@code null}, it must be a valid email address
+ * according to RFC 5322</li>
  * </ul>
  *
  * <p>
  * This constraint ensures that a field is "optional" (allows {@code null}) but
- * does not allow empty strings or strings consisting solely of whitespace.
+ * does not allow an invalid email address.
  * </p>
  */
 @Documented
 @Constraint(validatedBy = {})
-@Target({ ElementType.FIELD, ElementType.METHOD })
+@Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-// The regex \S ensures at least one non-whitespace character exists
 @Pattern(regexp = ".*\\S.*", message = "{attribute.not.blank}")
-public @interface NotBlankIfPresent {
-    String message() default "attribute.not.blank";
+@Email(message = "{attribute.not.email}")
+public @interface PCTSEmailValidation {
+    String message() default "{class}.{field} is not valid";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
