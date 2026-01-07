41e631c20dcd190102b701f86228ca536aa5c89e
Miguel Lehmann <lehmann@puzzle.ch>
2026-01-07 08:23:15 +0100

    fix: remove possible npe #44

--- backend/src/main/java/ch/puzzle/pcts/service/SecurityService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/SecurityService.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.Optional;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -64,8 +65,10 @@ public class SecurityService {
 
     private Collection<? extends GrantedAuthority> getAuthorities() {
         return Optional
-                .ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .of(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
                 .map(Authentication::getAuthorities)
                 .orElse(Collections.emptyList());
     }
+
 }
