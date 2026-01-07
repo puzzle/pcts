ec14bd13b6ad916f096024331a0d2106c7cdad8b
Manuel <moeri@puzzle.ch>
2026-01-07 13:50:26 +0100

    test(backend): Fix corresponding tests and test data  #50

--- backend/src/test/java/ch/puzzle/pcts/controller/MemberOverviewControllerIT.java
+++ backend/src/test/java/ch/puzzle/pcts/controller/MemberOverviewControllerIT.java
@@ -3,13 +3,18 @@ package ch.puzzle.pcts.controller;
 import static ch.puzzle.pcts.util.TestData.MEMBER_1_OVERVIEWS;
 import static ch.puzzle.pcts.util.TestData.MEMBER_1_OVERVIEW_DTO;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import ch.puzzle.pcts.SpringSecurityConfig;
+import ch.puzzle.pcts.dto.error.ErrorKey;
+import ch.puzzle.pcts.dto.error.FieldKey;
+import ch.puzzle.pcts.dto.error.GenericErrorDto;
 import ch.puzzle.pcts.dto.memberoverview.MemberOverviewDto;
+import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.mapper.MemberOverviewMapper;
 import ch.puzzle.pcts.model.memberoverview.MemberOverview;
 import ch.puzzle.pcts.service.business.MemberOverviewBusinessService;
@@ -18,16 +23,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -63,13 +69,36 @@ class MemberOverviewControllerIT {
         BDDMockito.given(mapper.toDto(memberOverviews)).willReturn(expectedDto);
 
         mvc
-                .perform(get(BASEURL + "/" + memberId)
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .accept(MediaType.APPLICATION_JSON))
+                .perform(get(BASEURL + "/" + memberId).with(csrf()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(JsonDtoMatcher.matchesDto(expectedDto, "$"));
 
         verify(service, times(1)).getById(memberId);
         verify(mapper, times(1)).toDto(memberOverviews);
+    }
+
+    @DisplayName("Should return 404 with NOT_FOUND error when member does not exist")
+    @Test
+    void shouldReturn404WhenMemberNotFound() throws Exception {
+        Long memberId = 99L;
+
+        Map<FieldKey, String> attributes = Map
+                .of(FieldKey.ENTITY, "Member", FieldKey.FIELD, "id", FieldKey.IS, memberId.toString());
+
+        GenericErrorDto error = new GenericErrorDto(ErrorKey.NOT_FOUND, attributes);
+
+        BDDMockito.given(service.getById(memberId)).willThrow(new PCTSException(HttpStatus.NOT_FOUND, List.of(error)));
+
+        mvc
+                .perform(get(BASEURL + "/" + memberId).with(csrf()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].key").value("NOT_FOUND"))
+                .andExpect(jsonPath("$[0].values.ENTITY").value("Member"))
+                .andExpect(jsonPath("$[0].values.FIELD").value("id"))
+                .andExpect(jsonPath("$[0].values.IS").value("99"));
+
+        verify(service).getById(memberId);
+        verifyNoInteractions(mapper);
     }
 }
