a5fda441b8f76702671cc87708461e4ea729b5bf
Manuel <moeri@puzzle.ch>
2026-01-09 10:24:56 +0100

    fix(model): Remove inheritation of model  #50

--- backend/src/main/java/ch/puzzle/pcts/service/validation/MemberOverviewValidationService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/validation/MemberOverviewValidationService.java
@@ -1,8 +1,21 @@
 package ch.puzzle.pcts.service.validation;
 
-import ch.puzzle.pcts.model.memberoverview.MemberOverview;
+import static ch.puzzle.pcts.service.validation.ValidationBase.buildGenericErrorDto;
+
+import ch.puzzle.pcts.dto.error.ErrorKey;
+import ch.puzzle.pcts.dto.error.FieldKey;
+import ch.puzzle.pcts.exception.PCTSException;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberOverviewValidationService extends ValidationBase<MemberOverview> {
+public class MemberOverviewValidationService {
+
+    public void validateOnGetById(Long id) {
+        if (id == null) {
+            throw new PCTSException(HttpStatus.BAD_REQUEST,
+                                    buildGenericErrorDto(ErrorKey.VALIDATION, Map.of(FieldKey.FIELD, "id")));
+        }
+    }
 }
