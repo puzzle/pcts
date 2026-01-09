5553a57d62bbf438f82d456cfcb6ea4ca8e25174
Manuel <moeri@puzzle.ch>
2026-01-09 11:01:34 +0100

    refactor(service): Change location of excpetion thrown from business service to persistence service  #50

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/MemberOverviewPersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/MemberOverviewPersistenceService.java
@@ -1,8 +1,16 @@
 package ch.puzzle.pcts.service.persistence;
 
+import static ch.puzzle.pcts.Constants.MEMBER_OVERVIEW;
+
+import ch.puzzle.pcts.dto.error.ErrorKey;
+import ch.puzzle.pcts.dto.error.FieldKey;
+import ch.puzzle.pcts.dto.error.GenericErrorDto;
+import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.model.memberoverview.MemberOverview;
 import ch.puzzle.pcts.repository.MemberOverviewRepository;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +23,25 @@ public class MemberOverviewPersistenceService {
     }
 
     public List<MemberOverview> getById(Long id) {
-        return repository.findAllByMemberId(id);
+        List<MemberOverview> result = repository.findAllByMemberId(id);
+
+        if (result.isEmpty()) {
+            throw new PCTSException(HttpStatus.NOT_FOUND,
+                                    List
+                                            .of(new GenericErrorDto(ErrorKey.NOT_FOUND,
+                                                                    Map
+                                                                            .of(FieldKey.ENTITY,
+                                                                                entityName(),
+                                                                                FieldKey.FIELD,
+                                                                                "id",
+                                                                                FieldKey.IS,
+                                                                                id.toString()))));
+        }
+
+        return result;
+    }
+
+    protected String entityName() {
+        return MEMBER_OVERVIEW;
     }
 }
