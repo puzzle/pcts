5a565fbc2f2e1e89ae4060f5ee9278310643d18f
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-01-09 16:53:47 +0100

    fix: resolve conversations #42

--- backend/src/main/java/ch/puzzle/pcts/model/calculation/CalculationChild.java
+++ backend/src/main/java/ch/puzzle/pcts/model/calculation/CalculationChild.java
@@ -2,7 +2,7 @@ package ch.puzzle.pcts.model.calculation;
 
 import ch.puzzle.pcts.model.Model;
 
-public interface CalculationChild extends Model {
+public interface CalculationChild {
     Calculation getCalculation();
     void setCalculation(Calculation calculation);
 }
