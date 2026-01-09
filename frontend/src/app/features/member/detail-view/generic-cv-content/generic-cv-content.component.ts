370bc956bfab4b8a9c37372439dc76b0ac4af971
Miguel Lehmann <lehmann@puzzle.ch>
2026-01-09 14:21:40 +0100

    feat: add first generic table #374

--- frontend/src/app/features/member/detail-view/generic-cv-content/generic-cv-content.component.ts
+++ frontend/src/app/features/member/detail-view/generic-cv-content/generic-cv-content.component.ts
@@ -1,11 +1,13 @@
-import { Component, input } from '@angular/core';
-import { ScopedTranslationPipe } from '../../../../shared/pipes/scoped-translation-pipe';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import { Component, effect, input, signal } from '@angular/core';
+import { GenCol, GenericTableDataSource } from '../../../../shared/generic-table/GenericTableDataSource';
 import { GenericTableComponent } from '../../../../shared/generic-table/generic-table.component';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { ScopedTranslationPipe } from '../../../../shared/pipes/scoped-translation-pipe';
 
 @Component({
   selector: 'app-generic-cv-content',
+  standalone: true,
   imports: [
     ScopedTranslationPipe,
     MatButton,
@@ -15,6 +17,30 @@ import { GenericTableComponent } from '../../../../shared/generic-table/generic-
   templateUrl: './generic-cv-content.component.html',
   styleUrl: './generic-cv-content.component.scss'
 })
-export class GenericCvContentComponent {
+export class GenericCvContentComponent<T extends object> {
   title = input.required<string>();
+
+  data = input.required<T[] | null>();
+
+  columns = input.required<GenCol<T>[]>();
+
+  tableDataSource = signal<GenericTableDataSource<T> | null>(null);
+
+  constructor() {
+    effect(() => {
+      const cols = this.columns();
+      const data = this.data();
+      if (data) {
+        if (this.tableDataSource()) {
+          this.tableDataSource()!.columnDefs = cols;
+          this.tableDataSource()!.data = data;
+        } else {
+          this.tableDataSource.set(new GenericTableDataSource<T>(cols, data));
+        }
+        console.log(this.tableDataSource());
+        console.log(this.columns());
+        console.log(this.data());
+      }
+    });
+  }
 }
---
+++ frontend/src/app/shared/generic-table/GenericTableDataSource.ts
@@ -0,0 +1,57 @@
+import { MatTableDataSource } from '@angular/material/table';
+
+
+type Formatter = (value: any) => any;
+
+
+export class GenCol<T> {
+  columnName = '';
+
+  getValue: (model: T) => any = () => {};
+
+  pipes: Formatter[] = [];
+
+  shouldLink = false;
+
+  protected constructor() {}
+
+  public static fromAttr<T>(field: keyof T, pipes: Formatter[] = []): GenCol<T> {
+    const genCol = new GenCol<T>();
+    genCol.getValue = (model: T) => model[field];
+    genCol.columnName = field.toString();
+    genCol.pipes = pipes;
+    return genCol;
+  }
+
+  public static fromCalculated<T>(columnName: string, getValue: (model: T) => any, pipes: Formatter[] = []): GenCol<T> {
+    const genCol = new GenCol<T>();
+    genCol.getValue = getValue;
+    genCol.columnName = columnName;
+    genCol.pipes = pipes;
+    return genCol;
+  }
+
+  public withLink(shouldLink = true) {
+    this.shouldLink = shouldLink;
+    return this;
+  }
+}
+
+
+export class GenericTableDataSource<T> extends MatTableDataSource<T> {
+  private _columnDefs: GenCol<T>[] = [];
+
+  constructor(columnDefs: GenCol<T>[], initialData?: T[]) {
+    super(initialData);
+    this.columnDefs = columnDefs;
+  }
+
+
+  get columnDefs(): GenCol<T>[] {
+    return this._columnDefs;
+  }
+
+  set columnDefs(value: GenCol<T>[]) {
+    this._columnDefs = value;
+  }
+}
