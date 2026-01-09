e4ad548230a994b25585d6f9b3d2b96251d3d23d
Manuel <moeri@puzzle.ch>
2026-01-09 09:13:53 +0100

    refactor(dto): Fix name of dto to have correct prefix  #50

--- backend/src/main/java/ch/puzzle/pcts/dto/memberoverview/MemberOverviewCvDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/memberoverview/MemberOverviewCvDto.java
@@ -6,7 +6,7 @@ import ch.puzzle.pcts.dto.memberoverview.experience.MemberOverviewExperienceDto;
 import ch.puzzle.pcts.dto.memberoverview.leadershipexperience.MemberOverviewLeadershipExperienceDto;
 import java.util.List;
 
-public record MemberCvDto(List<MemberOverviewDegreeDto> degrees, List<MemberOverviewExperienceDto> experiences,
+public record MemberOverviewCvDto(List<MemberOverviewDegreeDto> degrees, List<MemberOverviewExperienceDto> experiences,
         List<MemberOverviewCertificateDto> certificates,
         List<MemberOverviewLeadershipExperienceDto> leadershipExperiences) {
 }
