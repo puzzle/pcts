9be2a4406f526b90f9b600d1d7d548e01884dd80
Manuel <moeri@puzzle.ch>
2026-01-09 10:42:49 +0100

    fix(persistence): Turn repository private final  #50

--- backend/src/main/java/ch/puzzle/pcts/service/persistence/MemberOverviewPersistenceService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/persistence/MemberOverviewPersistenceService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberOverviewPersistenceService {
 
-    MemberOverviewRepository repository;
+    private final MemberOverviewRepository repository;
 
     public MemberOverviewPersistenceService(MemberOverviewRepository repository) {
         this.repository = repository;
