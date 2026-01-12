7b2dd0b6d01f6bc0c88bd6b0c80585bcac92ca61
Giannin <contact@giannin.ch>
2026-01-12 20:57:07 +0100

    test(auth): add tests for new validator #44

--- backend/src/test/java/ch/puzzle/pcts/util/validation/PCTSStringValidationTest.java
+++ backend/src/test/java/ch/puzzle/pcts/util/validation/PCTSStringValidationTest.java
@@ -1,14 +1,10 @@
-package ch.puzzle.pcts.util;
+package ch.puzzle.pcts.util.validation;
 
-import static org.apache.commons.lang3.StringUtils.trim;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import ch.puzzle.pcts.dto.error.FieldKey;
 import ch.puzzle.pcts.exception.PCTSException;
-import ch.puzzle.pcts.model.Model;
-import ch.puzzle.pcts.service.validation.ValidationBase;
-import ch.puzzle.pcts.util.validation.PCTSStringValidation;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -19,13 +15,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class PCTSStringValidationTest {
+class PCTSStringValidationTest extends CustomValidationTestBase {
     DummyValidationService service = new DummyValidationService();
 
     @DisplayName("Should throw error when string is null")
     @Test
     void shouldThrowErrorWhenStringIsNull() {
-        DummyClass invalid = new DummyClass(null);
+        DummyClass invalid = withString(null);
         PCTSException exception = assertThrows(PCTSException.class, () -> service.validate(invalid));
         assertEquals(List.of(Map.of(FieldKey.FIELD, "string", FieldKey.CLASS, "DummyClass")),
                      exception.getErrorAttributes());
@@ -35,7 +31,7 @@ class PCTSStringValidationTest {
     @ParameterizedTest
     @ValueSource(strings = { "", "  " })
     void shouldThrowErrorWhenStringIsBlank(String blank) {
-        DummyClass invalid = new DummyClass(blank);
+        DummyClass invalid = withString(blank);
         PCTSException exception = assertThrows(PCTSException.class, () -> service.validate(invalid));
 
         assertEquals(List.of(Map.of(FieldKey.FIELD, "string", FieldKey.CLASS, "DummyClass")),
@@ -46,7 +42,7 @@ class PCTSStringValidationTest {
     @ParameterizedTest
     @ValueSource(strings = { "S", "  S " })
     void shouldThrowErrorWhenStringIsTooShort(String shortString) {
-        DummyClass invalid = new DummyClass(shortString);
+        DummyClass invalid = withString(shortString);
         PCTSException exception = assertThrows(PCTSException.class, () -> service.validate(invalid));
 
         assertEquals(List
@@ -68,7 +64,7 @@ class PCTSStringValidationTest {
     @Test
     void shouldThrowErrorWhenStringIsTooLong() {
         String longString = "S".repeat(251);
-        DummyClass invalid = new DummyClass(longString);
+        DummyClass invalid = withString(longString);
         PCTSException exception = assertThrows(PCTSException.class, () -> service.validate(invalid));
 
         assertEquals(List
@@ -89,32 +85,7 @@ class PCTSStringValidationTest {
     @DisplayName("Should not throw error when string is valid")
     @Test
     void shouldNotThrowErrorWhenStringIsValid() {
-        DummyClass valid = new DummyClass("This is composed of valid characters");
+        DummyClass valid = withString("This is composed of valid characters");
         service.validate(valid);
-    }
-
-    private static class DummyClass implements Model {
-        @PCTSStringValidation
-        String string;
-
-        public DummyClass(String string) {
-            this.string = trim(string);
-        }
-
-        @Override
-        public Long getId() {
-            return 0L;
-        }
-
-        @Override
-        public void setId(Long id) {
-            // Isn't needed for the tests
-        }
-    }
-
-    private static class DummyValidationService extends ValidationBase<DummyClass> {
-        public DummyValidationService() {
-            super();
-        }
     }
 }
