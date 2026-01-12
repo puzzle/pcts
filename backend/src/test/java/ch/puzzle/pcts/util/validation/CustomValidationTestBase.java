9407c149ed8545ca991aaeb522a3b74fdfe0e24f
Giannin <contact@giannin.ch>
2026-01-12 20:57:07 +0100

    test(auth): add tests for new validator #44

---
+++ backend/src/test/java/ch/puzzle/pcts/util/validation/CustomValidationTestBase.java
@@ -0,0 +1,62 @@
+package ch.puzzle.pcts.util.validation;
+
+import static org.apache.commons.lang3.StringUtils.trim;
+
+import ch.puzzle.pcts.model.Model;
+import ch.puzzle.pcts.service.validation.ValidationBase;
+
+public class CustomValidationTestBase {
+    private static DummyClass createValid() {
+        DummyClass dummy = new DummyClass();
+        dummy.setEmail("email@example.com");
+        dummy.setString("This is a valid string");
+        return dummy;
+    }
+
+    protected static DummyClass withEmail(String email) {
+        DummyClass dummy = createValid();
+        dummy.setEmail(email);
+        return dummy;
+    }
+
+    protected static DummyClass withString(String string) {
+        DummyClass dummy = createValid();
+        dummy.setString(string);
+        return dummy;
+    }
+
+    protected static class DummyClass implements Model {
+        @PCTSStringValidation
+        String string;
+
+        @PCTSEmailValidation
+        String email;
+
+        public DummyClass() {
+        }
+
+        public void setEmail(String email) {
+            this.email = trim(email);
+        }
+
+        public void setString(String string) {
+            this.string = trim(string);
+        }
+
+        @Override
+        public Long getId() {
+            return 0L;
+        }
+
+        @Override
+        public void setId(Long id) {
+            // Isn't needed for the tests
+        }
+    }
+
+    protected static class DummyValidationService extends ValidationBase<DummyClass> {
+        public DummyValidationService() {
+            super();
+        }
+    }
+}
