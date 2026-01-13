0f438b2d3ebaa1cc126b124564c0df782b086b2d
Miguel Lehmann <lehmann@puzzle.ch>
2026-01-13 13:54:07 +0100

    feat: remove table when there is no data #374

--- frontend/src/app/features/member/detail-view/generic-cv-content/generic-cv-content.component.ts
+++ frontend/src/app/features/member/detail-view/generic-cv-content/generic-cv-content.component.ts
@@ -1,10 +1,9 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { GenCol, GenericTableDataSource } from '../../../../shared/generic-table/generic-table-data-source';
 import { GenericTableComponent } from '../../../../shared/generic-table/generic-table.component';
 import { ScopedTranslationPipe } from '../../../../shared/pipes/scoped-translation-pipe';
 import { ColumnTemplateDirective } from '../../../../shared/generic-table/column-template/column-template.directive';
 import { TypedTemplateDirective } from '../../../../shared/generic-table/type-template/typed-template.directive';
-import { I18N_PREFIX } from '../../../../shared/i18n-prefix.token';
 import { CrudButtonComponent } from '../../../../shared/crud-button/crud-button.component';
 
 @Component({
@@ -21,8 +20,6 @@ import { CrudButtonComponent } from '../../../../shared/crud-button/crud-button.
   styleUrl: './generic-cv-content.component.scss'
 })
 export class GenericCvContentComponent<T extends object> {
-  i18nPrefix = inject(I18N_PREFIX);
-
   data = input.required<T[] | null>();
 
   columns = input.required<GenCol<T>[]>();
@@ -45,7 +42,6 @@ export class GenericCvContentComponent<T extends object> {
   }
 
   getExperienceBadgeClass(type: string): string {
-    console.log(this.i18nPrefix);
     switch (type) {
       case 'Internship':
         return 'badge-internship';
