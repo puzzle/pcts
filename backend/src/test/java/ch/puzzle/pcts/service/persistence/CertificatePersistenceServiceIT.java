33e9371cd09482e05524b834dcc07571949e2cc2
Linus Ammon <ammon@puzzle.ch>
2026-01-20 08:17:03 +0100

    test: remove redundant stuffs

--- backend/src/test/java/ch/puzzle/pcts/service/persistence/CertificatePersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/pcts/service/persistence/CertificatePersistenceServiceIT.java
@@ -5,11 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import ch.puzzle.pcts.model.certificate.Certificate;
 import ch.puzzle.pcts.repository.CertificateRepository;
-import ch.puzzle.pcts.repository.CertificateTypeRepository;
-import ch.puzzle.pcts.repository.MemberRepository;
-import ch.puzzle.pcts.repository.OrganisationUnitRepository;
 import jakarta.transaction.Transactional;
-import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,13 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 class CertificatePersistenceServiceIT
         extends
             PersistenceBaseIT<Certificate, CertificateRepository, CertificatePersistenceService> {
-
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private CertificateTypeRepository certificateTypeRepository;
-    @Autowired
-    private OrganisationUnitRepository organisationUnitRepository;
 
     @Autowired
     CertificatePersistenceServiceIT(CertificatePersistenceService service) {
@@ -45,14 +34,7 @@ class CertificatePersistenceServiceIT
     @DisplayName("Should get leadership experience when kind is not CERTIFICATE")
     @Transactional
     void shouldGetLeadershipExperience() {
-        Certificate certificate = Certificate.Builder
-                .builder()
-                .withMember(MEMBER_2)
-                .withCertificateType(LEADERSHIP_TYPE_2)
-                .withCompletedAt(LocalDate.now())
-                .build();
-
-        Certificate savedCertificate = persistenceService.save(certificate);
+        Certificate savedCertificate = persistenceService.save(LEADERSHIP_CERT_1);
         Certificate result = assertDoesNotThrow(() -> persistenceService
                 .findLeadershipExperience(savedCertificate.getId()));
 
