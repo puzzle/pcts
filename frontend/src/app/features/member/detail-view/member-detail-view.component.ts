0ba250d9ee39f1694f05092a8ba9e6d8e834c116
Yanick Minder <minder@puzzle.ch>
2026-01-21 13:50:27 +0100

    refactor(frontend): refactor to prevent ussage of noIdCertificate  #306

--- frontend/src/app/features/member/detail-view/member-detail-view.component.ts
+++ frontend/src/app/features/member/detail-view/member-detail-view.component.ts
@@ -21,8 +21,9 @@ import { MemberOverviewModel } from '../member-overview.model';
 import { AddCertificateComponent } from '../modal-components/add-certificate.component/add-certificate.component';
 import { ModalService } from '../../../shared/modal-service';
 import { ModalSubmitMode } from '../../../shared/enum/modal-submit-mode.enum';
-import { CertificateService, NoIdCertificate } from '../../certificates/certificate.service';
+import { CertificateService } from '../../certificates/certificate.service';
 import { MemberModel } from '../member.model';
+import { CertificateModel } from '../../certificates/certificate.model';
 import { RolePointsModel } from './RolePointsModel';
 import { MemberCalculationTableComponent } from './calculation-table/member-calculation-table.component';
 
@@ -106,7 +107,7 @@ export class MemberDetailViewComponent implements OnInit {
       });
   }
 
-  openCertificateDialog(model?: NoIdCertificate) {
+  openCertificateDialog(model?: CertificateModel) {
     this.dialog.openModal(AddCertificateComponent, { data: model })
       .afterSubmitted
       .subscribe(({ modalSubmitMode, submittedModel }) => {
