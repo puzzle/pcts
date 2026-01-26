147917e94c5d0723549ff055f1f047885f0d61cc
Giannin <contact@giannin.ch>
2026-01-26 20:17:45 +0100

    feat(auth): add correct authentication parameters to overview controller #44

--- backend/src/main/java/ch/puzzle/pcts/controller/MemberOverviewController.java
+++ backend/src/main/java/ch/puzzle/pcts/controller/MemberOverviewController.java
@@ -3,7 +3,7 @@ package ch.puzzle.pcts.controller;
 import ch.puzzle.pcts.dto.memberoverview.MemberOverviewDto;
 import ch.puzzle.pcts.mapper.MemberOverviewMapper;
 import ch.puzzle.pcts.model.memberoverview.MemberOverview;
-import ch.puzzle.pcts.security.annotation.IsAdmin;
+import ch.puzzle.pcts.security.annotation.IsAdminOrOwner;
 import ch.puzzle.pcts.service.business.MemberOverviewBusinessService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -13,12 +13,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@IsAdmin
+@IsAdminOrOwner
 @RestController
 @RequestMapping("/api/v1/member-overviews")
 @Tag(name = "member-overviews", description = "Get the member and everything associated with the member")
@@ -36,7 +37,7 @@ public class MemberOverviewController {
             @Content(mediaType = "application/json", schema = @Schema(implementation = MemberOverviewDto.class)) })
     @GetMapping("{memberId}")
     public ResponseEntity<MemberOverviewDto> getMemberOverviewByMemberId(@Parameter(description = "The ID of the member whose overview should be retrieved.", required = true)
-    @PathVariable Long memberId) {
+    @PathVariable @P("id") Long memberId) {
         List<MemberOverview> memberOverviews = memberOverviewBusinessService.getById(memberId);
         return ResponseEntity.ok(memberOverviewMapper.toDto(memberOverviews));
     }
