2ab33568a8171ef673719df1380d81409b190a8d
Manuel <moeri@puzzle.ch>
2026-01-28 08:49:23 +0100

    test(business): Add multi role complex person as test as well  #408

--- backend/src/test/java/ch/puzzle/pcts/service/business/CalculationBusinessServiceIT.java
+++ backend/src/test/java/ch/puzzle/pcts/service/business/CalculationBusinessServiceIT.java
@@ -18,22 +18,35 @@ class CalculationBusinessServiceIT {
     CalculationBusinessService calcBusinessService;
 
     @Transactional
-    @DisplayName("Should calculate the points of John Doe correctly")
+    @DisplayName("Should calculate the points for a simple member (John Doe) correctly")
     @Test
     @Sql({ "/db/test-data-migration/db-scripts/alter__sequences.sql",
             "/db/test-data-migration/db-scripts/insert__john_doe.sql" })
-    void shouldCalculateJohnDoe() {
+    void shouldCalculateSimpleMemberCorrectly() {
         assertThat(calcBusinessService.getById(1000L).getPoints().setScale(1, RoundingMode.FLOOR))
                 .isEqualTo(BigDecimal.valueOf(15.7));
     }
 
     @Transactional
-    @DisplayName("Should calculate the points of Hermine Sidney correctly")
+    @DisplayName("Should calculate the points for a complex member (Hermine Sidney) correctly")
     @Test
     @Sql({ "/db/test-data-migration/db-scripts/alter__sequences.sql",
             "/db/test-data-migration/db-scripts/insert__hermine_sidney.sql" })
-    void shouldCalculateHermineSidney() {
+    void shouldCalculateComplexMemberCorrectly() {
         assertThat(calcBusinessService.getById(1000L).getPoints().setScale(1, RoundingMode.FLOOR))
                 .isEqualTo(BigDecimal.valueOf(95.7));
+    }
+
+    @Transactional
+    @DisplayName("Should calculate the points for a complex multi-role member (Devon Ricky) correctly")
+    @Test
+    @Sql({ "/db/test-data-migration/db-scripts/alter__sequences.sql",
+            "/db/test-data-migration/db-scripts/insert__devon_ricky.sql" })
+    void shouldCalculateComplexMultiRoleMemberCorrectly() {
+        assertThat(calcBusinessService.getById(1000L).getPoints().setScale(1, RoundingMode.FLOOR))
+                .isEqualTo(BigDecimal.valueOf(87.5));
+
+        assertThat(calcBusinessService.getById(1001L).getPoints().setScale(1, RoundingMode.FLOOR))
+                .isEqualTo(BigDecimal.valueOf(83.9));
     }
 }
