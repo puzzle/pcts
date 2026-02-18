668461ecc63fbb17b27cc3e163888640c4ea52a1
Giannin <puzzle@giannin.ch>
2026-02-18 11:38:39 +0100

    refactor: remove unused import #44

--- backend/src/test/java/ch/puzzle/pcts/service/business/MemberBusinessServiceTest.java
+++ backend/src/test/java/ch/puzzle/pcts/service/business/MemberBusinessServiceTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
