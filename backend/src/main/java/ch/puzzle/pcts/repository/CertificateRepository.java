221daa83f2a517ab67e764cd046b3a67e4c80313
Manuel <moeri@puzzle.ch>
2026-03-09 11:25:34 +0100

    fix(repo): Remove a couple of unused jpa queries  #517

--- backend/src/main/java/ch/puzzle/pcts/repository/CertificateRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/CertificateRepository.java
@@ -1,10 +1,8 @@
 package ch.puzzle.pcts.repository;
 
 import ch.puzzle.pcts.model.certificate.Certificate;
-import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CertificateRepository extends SoftDeleteRepository<Certificate, Long> {
-    Optional<Certificate> findByIdAndCertificateType(Long id);
 }
--- backend/src/main/java/ch/puzzle/pcts/repository/LeadershipExperienceTypeRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/LeadershipExperienceTypeRepository.java
@@ -1,15 +1,12 @@
 package ch.puzzle.pcts.repository;
 
 import ch.puzzle.pcts.model.leadershipexperiencetype.LeadershipExperienceType;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeadershipExperienceTypeRepository extends SoftDeleteRepository<LeadershipExperienceType, Long> {
     Optional<LeadershipExperienceType> findByName(String name);
-
-    List<LeadershipExperienceType> findAllOfLeadershipExperienceType();
 
     Optional<LeadershipExperienceType> findByIdNotAndDeletedAtIsNull(Long id);
 
