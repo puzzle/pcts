d5725a4b9587d8357804764378f574eb8fd8fe65
Manuel <moeri@puzzle.ch>
2026-03-10 11:19:43 +0100

    test(architecture): Resolve all remaining architecture problems  #517

--- backend/src/main/java/ch/puzzle/pcts/repository/LeadershipExperienceRepository.java
+++ backend/src/main/java/ch/puzzle/pcts/repository/LeadershipExperienceRepository.java
@@ -1,7 +1,9 @@
 package ch.puzzle.pcts.repository;
 
 import ch.puzzle.pcts.model.leadershipexperience.LeadershipExperience;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LeadershipExperienceRepository extends SoftDeleteRepository<LeadershipExperience, Long> {
 
 }
