eee1644bcd686b387a4e2acbeba8672f48905cac
Giannin <puzzle@giannin.ch>
2026-05-20 19:43:02 +0200

    chore: run formatter #44

--- backend/src/main/java/ch/puzzle/pcts/PctsApplication.java
+++ backend/src/main/java/ch/puzzle/pcts/PctsApplication.java
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @EnableScheduling
