fde9fdefb9c87d88df476341596aaee33d4c0bba
Giannin <contact@giannin.ch>
2026-05-21 19:24:30 +0200

    test: add tests for new feature #514

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,23 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
-import { provideTranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
+
 jest.mock('@puzzleitc/puzzle-shell', () => jest.fn());
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let document: Document;
+  let translateService: TranslateService;
+
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
-      providers: [provideTranslateService(),
-        provideRouter([])]
+      imports: [AppComponent,
+        TranslateModule.forRoot()],
+      providers: [provideRouter([])]
     })
       .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    document = TestBed.inject(DOCUMENT);
+    translateService = TestBed.inject(TranslateService);
+  });
+
+  afterEach(() => {
+    document.documentElement.removeAttribute('lang');
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app)
       .toBeTruthy();
+  });
+
+  it('should set default lang attribute', () => {
+    fixture.detectChanges();
+
+    expect(document.documentElement.getAttribute('lang'))
+      .toBe('en');
+  });
+
+  it('should update the lang attribute', () => {
+    fixture.detectChanges();
+
+    translateService.use('de');
+    fixture.detectChanges();
+
+    expect(document.documentElement.getAttribute('lang'))
+      .toBe('de');
   });
 });
