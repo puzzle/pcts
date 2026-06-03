c698b92c7d908e8cae2bbd686746e96677a150e7
Giannin <puzzle@giannin.ch>
2026-06-03 13:46:03 +0200

    fix: correct keycloak URL #44

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://pcts-backend:8080',
   keycloak: {
-    url: 'https://sso.puzzle.ch',
+    url: 'https://sso.puzzle.ch/auth',
     realm: 'pitc',
     clientId: 'pitc_pcts_staging'
   }
