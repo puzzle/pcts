c553367c2bd030c4e920f32c9843a441348baca6
Giannin <contact@giannin.ch>
2026-06-20 14:50:13 +0200

    chore: ignore missing import for linting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,8 @@ import { buildAppConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { RuntimeConfig } from './app/core/runtime-config.model';
 
+// This should be fine, as fetch is always available through the browser
+// eslint-disable-next-line no-undef
 fetch('/config.json')
   .then((res) => res.json())
   .then((config: RuntimeConfig) => bootstrapApplication(AppComponent, buildAppConfig(config)))
