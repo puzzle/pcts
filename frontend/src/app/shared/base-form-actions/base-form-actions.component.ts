218dce7cb88dbe61cc199c4648148e559b4c26a5
Manuel <moeri@puzzle.ch>
2026-07-03 11:42:46 +0200

    fix(base-form-action): make button correctly update based on form validity

--- frontend/src/app/shared/base-form-actions/base-form-actions.component.ts
+++ frontend/src/app/shared/base-form-actions/base-form-actions.component.ts
@@ -2,6 +2,8 @@ import { Component, input, output } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { ScopedTranslationPipe } from '../pipes/scoped-translation-pipe';
 import { FormGroup } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-form-actions',
@@ -14,4 +16,6 @@ export class BaseFormActionsComponent {
   formGroup = input.required<FormGroup>();
 
   canceled = output();
+
+  isInvalid = toSignal(this.formGroup().statusChanges.pipe(map((f) => f === 'INVALID')));
 }
