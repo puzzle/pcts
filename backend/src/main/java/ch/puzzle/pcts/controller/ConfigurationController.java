3f9ca7fa99c056f2edd095d23812f039a1bfac00
Flynn Streit <streit@puzzle.ch>
2026-07-07 08:37:16 +0200

    feat(backend): change the old enpoint url and add a new endpoint for fetching the url  #541

--- backend/src/main/java/ch/puzzle/pcts/controller/ConfigurationController.java
+++ backend/src/main/java/ch/puzzle/pcts/controller/ConfigurationController.java
@@ -1,8 +1,11 @@
 package ch.puzzle.pcts.controller;
 
 import ch.puzzle.pcts.configuration.AuthorizationConfiguration;
+import ch.puzzle.pcts.configuration.HelpUrlConfiguration;
 import ch.puzzle.pcts.dto.configuration.ConfigurationDto;
+import ch.puzzle.pcts.dto.support.SupportDto;
 import ch.puzzle.pcts.mapper.ConfigurationMapper;
+import ch.puzzle.pcts.mapper.SupportMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/configuration")
 @Tag(name = "configuration", description = "Retrieve configuration properties for the application")
 public class ConfigurationController {
-    private final ConfigurationMapper mapper;
+    private final ConfigurationMapper configMapper;
+    private final SupportMapper supportMapper;
     private final AuthorizationConfiguration authConfig;
+    private final HelpUrlConfiguration helpUrlConfig;
 
-    public ConfigurationController(ConfigurationMapper mapper, AuthorizationConfiguration authConfig) {
-        this.mapper = mapper;
+    public ConfigurationController(ConfigurationMapper configMapper, SupportMapper supportMapper,
+                                   AuthorizationConfiguration authConfig, HelpUrlConfiguration helpUrlConfig) {
+        this.configMapper = configMapper;
+        this.supportMapper = supportMapper;
         this.authConfig = authConfig;
+        this.helpUrlConfig = helpUrlConfig;
     }
 
-    @GetMapping
+    @GetMapping("/authorization")
     public ResponseEntity<ConfigurationDto> getAuthorizationConfiguration() {
-        return ResponseEntity.ok(mapper.toDto(this.authConfig));
+        return ResponseEntity.ok(configMapper.toDto(this.authConfig));
+    }
+
+    @GetMapping("/help")
+    public ResponseEntity<SupportDto> getHelpUrl() {
+        return ResponseEntity.ok(supportMapper.toDto(this.helpUrlConfig.helpUrl()));
     }
 }
