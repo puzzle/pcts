bac56f44a51548dc4f6a43631ff621f67b7b54e4
Flynn Streit <streit@puzzle.ch>
2026-07-07 08:43:54 +0200

    feat(frontend): add a fetch for the new endpoint which provides the support page url  #541

--- frontend/src/app/features/configuration/configuration.service.ts
+++ frontend/src/app/features/configuration/configuration.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ConfigurationModel } from './configuration.model';
+import { helpUrlModel } from './helpUrl.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class ConfigurationService {
   private readonly API_URL = '/api/v1/configuration';
 
   getConfiguration(): Observable<ConfigurationModel> {
-    return this.httpClient.get<ConfigurationModel>(this.API_URL);
+    return this.httpClient.get<ConfigurationModel>(this.API_URL + '/authorization');
+  }
+
+  getHelpUrl(): Observable<helpUrlModel> {
+    return this.httpClient.get<helpUrlModel>(this.API_URL + '/help');
   }
 }
