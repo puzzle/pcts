b2cf876d412a2ede4ab54590c29fbce08bdc76f8
Flynn Streit <streit@puzzle.ch>
2026-07-08 10:56:24 +0200

    test(frontend): add e2e test  #541

--- frontend/cypress/e2e/topbar.cy.ts
+++ frontend/cypress/e2e/topbar.cy.ts
@@ -3,15 +3,11 @@ import * as users from '../fixtures/users.json';
 describe('Topbar', () => {
   beforeEach(() => {
     cy.loginAsUser(users.gl);
+    cy.visit('/');
   });
 
   it('should have correct url on help', () => {
-    cy.getByTestId('support-page-url')
-      .should('have.attr', 'href')
-      .then((href) => {
-        cy.visit(href);
-      });
-    cy.url()
-      .should('eq', 'https://google.com');
+    cy.get('#support-page-url')
+      .should('have.attr', 'href', 'https://dummy-url.test');
   });
 });
