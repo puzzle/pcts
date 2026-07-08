1048e852118f946ea0b45d567a1fac6eaf6320d7
Flynn Streit <streit@puzzle.ch>
2026-07-08 09:13:03 +0200

    fix(backend): fix naming of configuration property  #541

--- backend/src/main/java/ch/puzzle/pcts/configuration/HelpUrlConfiguration.java
+++ backend/src/main/java/ch/puzzle/pcts/configuration/HelpUrlConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-@ConfigurationProperties(prefix = "app.help")
+@ConfigurationProperties(prefix = "pcts.app.help")
 public record HelpUrlConfiguration(@NotBlank String url) {
     public String helpUrl() {
         if (isValid()) {
