f10b6649a1b9c6b5638a460a919ab7c7ffdb80a7
Flynn Streit <streit@puzzle.ch>
2026-07-08 14:23:15 +0200

    refactor(backend): remove check and add annotation @URL  #541

--- backend/src/main/java/ch/puzzle/pcts/configuration/AppConfiguration.java
+++ backend/src/main/java/ch/puzzle/pcts/configuration/AppConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "pcts.app")
-public record AppConfiguration(@NotBlank @URL String helpurl) {
+public record AppConfiguration(@NotBlank @URL(regexp = "") String helpurl) {
     public String helpUrl() {
         return helpurl;
     }
