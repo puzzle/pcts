9d94dd132b9ffaa4a27cbcd419b59adfa5bc31b9
Livio Canobbio <canobbio@puzzle.ch>
2026-08-04 11:51:26 +0200

    feat: add runable poc #712

---
+++ pcts-migration-poc/src/main/java/ch/puzzle/pctsmigrationpoc/PctsMigrationPocApplication.java
@@ -0,0 +1,13 @@
+package ch.puzzle.pctsmigrationpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PctsMigrationPocApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PctsMigrationPocApplication.class, args);
+    }
+
+}
