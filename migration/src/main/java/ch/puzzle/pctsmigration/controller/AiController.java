0b8e6ccc5ae255aa7c25203cdc4b01493ecaafb6
Livio Canobbio <canobbio@puzzle.ch>
2026-08-05 16:44:07 +0200

    feat: add pcts clients  #712

--- migration/src/main/java/ch/puzzle/pctsmigration/controller/AiController.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/controller/AiController.java
@@ -3,14 +3,26 @@ package ch.puzzle.pctsmigration.controller;
 import ch.puzzle.pctsmigration.model.MovieReview;
 import ch.puzzle.pctsmigration.model.OdsAnalysisResult;
 import ch.puzzle.pctsmigration.service.AiService;
+import ch.puzzle.pctsmigration.service.CertificateApiService;
 import ch.puzzle.pctsmigration.service.OdsParserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.api.CertificatesApi;
+import org.openapitools.client.model.CertificateDto;
+import org.openapitools.client.model.CertificateTypeDto;
+import org.openapitools.client.model.DegreeDto;
+import org.openapitools.client.model.DegreeTypeDto;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ai")
@@ -19,10 +31,12 @@ public class AiController {
 
     private final AiService aiService;
     private final OdsParserService odsParserService;
+    private final CertificateApiService certificateApiService;
 
-    public AiController(AiService aiService, OdsParserService odsParserService) {
+    public AiController(AiService aiService, OdsParserService odsParserService, CertificateApiService certificateApiService) {
         this.aiService = aiService;
         this.odsParserService = odsParserService;
+        this.certificateApiService = certificateApiService;
     }
 
     @PostMapping(value = "/prompt", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,5 +50,10 @@ public class AiController {
     public OdsAnalysisResult analyzeOds(@RequestPart("file") MultipartFile file) throws IOException {
         var parsed = odsParserService.parse(file);
         return aiService.analyzeOds(odsParserService.toPromptText(parsed));
+    }
+
+    @GetMapping("pctsApi")
+    public List<DegreeTypeDto> getFromPctsApi() throws ApiException {
+        return certificateApiService.getCertificates();
     }
 }
