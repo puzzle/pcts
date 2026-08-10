7d93934a4a308f03f8831ae85fee62c74c205a0c
Flynn Streit <streit@puzzle.ch>
2026-08-10 16:16:24 +0200

    feat(frontend): add role attribute to frontend member model  #581

--- frontend/src/app/features/member/member.model.ts
+++ frontend/src/app/features/member/member.model.ts
@@ -1,5 +1,6 @@
 import { OrganisationUnitModel } from '../organisation-unit/organisation-unit.model';
 import { EmploymentState } from '../../shared/enum/employment-state.enum';
+import { RoleModel } from '../roles/RoleModel';
 
 export interface MemberModel {
   id: number;
@@ -8,6 +9,7 @@ export interface MemberModel {
   birthDate: Date;
   abbreviation: string | null;
   employmentState: EmploymentState;
+  role: RoleModel;
   organisationUnit: OrganisationUnitModel;
   dateOfHire: Date | null;
 }
