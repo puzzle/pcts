5d5666e1be362701e7dba6024d96eebe0047020f
Livio Canobbio <canobbio@puzzle.ch>
2026-08-10 15:32:43 +0200

    refactor: clean up servie and piplen  #712

--- migration/src/main/java/ch/puzzle/pctsmigration/extractor/ExtractorService.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/extractor/ExtractorService.java
@@ -8,7 +8,6 @@ import org.springframework.web.multipart.MultipartFile;
 import jakarta.validation.Validator;
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ExtractorService {
@@ -24,21 +23,21 @@ public class ExtractorService {
 
     public <C, R, D> List<D> extract(MultipartFile file, ExtractionPipeline<C, R, D> pipeline) throws IOException, ApiException {
         String parsedToMarkdown = this.odsParserService.toPromptText(this.odsParserService.parse(file));
+
         R result = this.aiService.extractCertificateData(
                 parsedToMarkdown,
                 pipeline.systemPrompt(pipeline.fetchContext()),
-                pipeline.entityClass());
-
-        String abbreviation = Objects.requireNonNull(file.getOriginalFilename()).split("_")[0];
-        List<D> finalResult = pipeline.mapToDto(abbreviation.toUpperCase()).apply(result);
+                pipeline.entityClass()
+        );
 
         if (pipeline.validate()) {
             var violations = validator.validate(result);
             if (!violations.isEmpty()) {
-                throw new IllegalArgumentException("LLM generated invalid rating bounds: " + violations);
+                throw new IllegalArgumentException("LLM generated invalid data: " + violations);
             }
         }
 
-        return finalResult;
+        String filename = file.getOriginalFilename() != null ? file.getOriginalFilename() : "";
+        return pipeline.mapToDto(filename, result);
     }
 }
