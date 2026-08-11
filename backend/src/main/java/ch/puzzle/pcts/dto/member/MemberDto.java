0f179c49104e9e6f30f7155e1c0d1025ed7e6fc9
Flynn Streit <streit@puzzle.ch>
2026-08-11 15:28:05 +0200

    feat(backend): add list with roles to member as attribute  #581

--- backend/src/main/java/ch/puzzle/pcts/dto/member/MemberDto.java
+++ backend/src/main/java/ch/puzzle/pcts/dto/member/MemberDto.java
@@ -2,9 +2,11 @@ package ch.puzzle.pcts.dto.member;
 
 import ch.puzzle.pcts.dto.organisationunit.OrganisationUnitDto;
 import ch.puzzle.pcts.model.member.EmploymentState;
+import ch.puzzle.pcts.model.role.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record MemberDto(
         @Schema(description = "The unique identifier of the member.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_ONLY) Long id,
@@ -15,6 +17,7 @@ public record MemberDto(
         @Schema(description = "The member's hire date.", example = "2025-09-24", requiredMode = Schema.RequiredMode.NOT_REQUIRED) LocalDate dateOfHire,
         @Schema(description = "The member's birth date.", example = "1995-02-19", requiredMode = Schema.RequiredMode.REQUIRED) LocalDate birthDate,
         @Schema(description = "The Organisation unit of the member.", requiredMode = Schema.RequiredMode.NOT_REQUIRED) OrganisationUnitDto organisationUnit,
+        @Schema(description = "The roles a member fulfills.", example = "Intern, Developer", requiredMode = Schema.RequiredMode.NOT_REQUIRED) List<Role> roles,
         @Schema(description = "The unique identifier of the member from the PuzzleTime", example = "1", requiredMode = Schema.RequiredMode.NOT_REQUIRED) Long ptimeId,
         @Schema(description = "The timestamp of the last time the sync with the PuzzleTime API was successful", example = "2024-02-14T10:15:30", requiredMode = Schema.RequiredMode.NOT_REQUIRED) LocalDateTime lastSuccessfulSync,
         @Schema(description = "The amount of times the sync failed since it was last successful ", example = "3", requiredMode = Schema.RequiredMode.NOT_REQUIRED) Integer syncErrorCount) {
