240b250f1af5a52068fa2881a89d8b521855abe7
Flynn Streit <streit@puzzle.ch>
2026-08-17 07:41:29 +0200

    feat: change roles attribute to roleids on memberdtos and models  #581

--- frontend/src/app/features/member/member.model.ts
+++ frontend/src/app/features/member/member.model.ts
@@ -1,6 +1,5 @@
 import { OrganisationUnitModel } from '../organisation-unit/organisation-unit.model';
 import { EmploymentState } from '../../shared/enum/employment-state.enum';
-import { RoleModel } from '../roles/RoleModel';
 
 export interface MemberModel {
   id: number;
@@ -9,7 +8,7 @@ export interface MemberModel {
   birthDate: Date;
   abbreviation: string | null;
   employmentState: EmploymentState;
-  roles: RoleModel[];
   organisationUnit: OrganisationUnitModel;
   dateOfHire: Date | null;
+  roleIds: number[];
 }
