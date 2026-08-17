79970fb85b93daf355240a602d901cae1cd36b9c
Flynn Streit <streit@puzzle.ch>
2026-08-17 15:50:29 +0200

    feat(backend): write toDtos method

--- backend/src/main/java/ch/puzzle/pcts/service/business/RoleBusinessService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/business/RoleBusinessService.java
@@ -1,8 +1,11 @@
 package ch.puzzle.pcts.service.business;
 
+import ch.puzzle.pcts.dto.role.RoleDto;
 import ch.puzzle.pcts.model.role.Role;
 import ch.puzzle.pcts.service.persistence.RolePersistenceService;
 import ch.puzzle.pcts.service.validation.RoleValidationService;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,13 @@ public class RoleBusinessService extends BusinessBase<Role> {
 
     public List<Role> getAll() {
         return persistenceService.getAll();
+    }
+
+    public List<RoleDto> toDtos(List<Role> roles) {
+        List<RoleDto> dtos = new ArrayList<>();
+        for (Role role : roles) {
+            dtos.add(new RoleDto(role.getId(), role.getName(), role.getIsManagement()));
+        }
+        return dtos;
     }
 }
