7c0c10691533f1236fef504142886f493812a4fd
Flynn Streit <streit@puzzle.ch>
2026-08-25 09:39:10 +0200

    test(frontend): add test for role service  #581

--- frontend/src/app/features/roles/role.service.spec.ts
+++ frontend/src/app/features/roles/role.service.spec.ts
@@ -1,17 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RoleService } from './role.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { RoleModel } from './RoleModel';
+import { role1, role2 } from '../../shared/test/test-data';
 
 describe('RoleService', () => {
   let service: RoleService;
+  let httpMock: HttpTestingController;
+  const API_URL = '/api/v1/roles';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [RoleService,
+        provideHttpClient(),
+        provideHttpClientTesting()]
+    })
+      .compileComponents();
     service = TestBed.inject(RoleService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    expect(service)
+      .toBeTruthy();
   });
 
   it('should be created', () => {
     expect(service)
       .toBeTruthy();
+  });
+
+  describe('getAllRoles', () => {
+    it('should call httpClient.get with the correct URL and return roles', () => {
+      const mockRoles: RoleModel[] = [role1,
+        role2];
+
+      service.getAllRoles()
+        .subscribe((roles) => {
+          expect(roles)
+            .toEqual(mockRoles);
+        });
+
+      const req = httpMock.expectOne(`${API_URL}`);
+      expect(req.request.method)
+        .toBe('GET');
+      req.flush(mockRoles);
+    });
   });
 });
