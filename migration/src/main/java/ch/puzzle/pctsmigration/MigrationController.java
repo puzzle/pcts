ae4318ff07fcbe26035300230a20e2747d593ad2
Flynn Streit <streit@puzzle.ch>
2026-09-04 13:57:05 +0200

    feat(backend): add new endpoint to migration controller for submitting a leadershipexperience ods data file  #717

--- migration/src/main/java/ch/puzzle/pctsmigration/MigrationController.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/MigrationController.java
@@ -5,6 +5,7 @@ import ch.puzzle.pctsmigration.certificates.MultipleFileResultDto;
 import ch.puzzle.pctsmigration.exception.FileError;
 import ch.puzzle.pctsmigration.exception.MigrationException;
 import ch.puzzle.pctsmigration.extractor.ExtractorService;
+import ch.puzzle.pctsmigration.leadershipexperience.LeadershipExperienceExtractionPipeline;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.openapitools.client.model.CertificateInputDto;
+import org.openapitools.client.model.LeadershipExperienceInputDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +27,12 @@ public class MigrationController {
 
     private final ExtractorService service;
     private final CertificateExtractionPipeline certificateExtractionPipeline;
+    private final LeadershipExperienceExtractionPipeline leadershipExperienceExtractionPipeline;
 
-    public MigrationController(ExtractorService service, CertificateExtractionPipeline certificateExtractionPipeline) {
+    public MigrationController(ExtractorService service, CertificateExtractionPipeline certificateExtractionPipeline, LeadershipExperienceExtractionPipeline leadershipExperienceExtractionPipeline) {
         this.service = service;
         this.certificateExtractionPipeline = certificateExtractionPipeline;
+        this.leadershipExperienceExtractionPipeline = leadershipExperienceExtractionPipeline;
     }
 
     @PostMapping(value = "/certificate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -62,6 +66,18 @@ public class MigrationController {
                 result.addToFailedFiles(new FileError(filename, e.getMessage()));
             }
         }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @PostMapping(value = "/leadershipexperience", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Extract and migrate leadership experiences from an ODS file", description = "Uploads a legacy .ods spreadsheet containing leadership experience data. The system uses an AI-based extraction pipeline to parse the file, generate the corresponding `LeadershipExperienceInputDto` objects, and automatically persists them in the upstream pcts-api. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Leadership Experience successfully extracted and created in pcts-api.") })
+    public ResponseEntity<List<LeadershipExperienceInputDto>> leadershipExperience(@Parameter(description = "The .ods file containing the certificate data to be migrated. Must be a valid OpenDocument Spreadsheet.", required = true)
+    @RequestPart("file") MultipartFile file) {
+        List<LeadershipExperienceInputDto> result = service.extract(file, leadershipExperienceExtractionPipeline);
+        leadershipExperienceExtractionPipeline.create(result);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
