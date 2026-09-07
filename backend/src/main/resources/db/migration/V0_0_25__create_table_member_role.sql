1692d9752eee588b23ae238ca24491e667e6b836
Flynn Streit <streit@puzzle.ch>
2026-09-07 12:06:02 +0200

    feat(db): create migration script for member role  #581

--- backend/src/main/resources/db/migration/V0_0_25__create_table_member_role.sql
+++ backend/src/main/resources/db/migration/V0_0_25__create_table_member_role.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS member_role
 (                                   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
                                     member_id BIGINT NOT NULL,
                                     role_id BIGINT NOT NULL,
+                                    UNIQUE (member_id, role_id),
                                     CONSTRAINT member_role_fk_1
                                         FOREIGN KEY (member_id) REFERENCES member (id),
                                     CONSTRAINT member_role_fk_2
