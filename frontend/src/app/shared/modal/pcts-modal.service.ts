72105a9ceb2ff0b807465e37272ee6d63d3b57ee
schiltpzzle <schilt@puzzle.ch>
2026-09-08 11:18:56 +0200

    feat: Add provideModalI18nPrefix method for handling modal prefix #798

--- frontend/src/app/shared/modal/pcts-modal.service.ts
+++ frontend/src/app/shared/modal/pcts-modal.service.ts
@@ -5,7 +5,7 @@ import { enrichMatDialogRef, StrictlyTypedDialog, TypedMatDialogRef } from './st
 import { ModalSubmitMode } from '../enum/modal-submit-mode.enum';
 import { PctsModalBuilder } from './pcts-modal-builder';
 
-export interface PCTSDialogConfig<T> {
+export interface PCTSDialogConfig<T extends { id: number }> {
   model: T | undefined;
   submitOptions: ModalSubmitMode[];
 }
