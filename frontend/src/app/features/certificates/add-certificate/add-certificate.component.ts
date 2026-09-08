c3d772c6142765e0c6087a7fe700cc24a3d8e53c
schiltpzzle <schilt@puzzle.ch>
2026-09-08 11:20:39 +0200

    feat: update modals to use prefix method #798

--- frontend/src/app/features/certificates/add-certificate/add-certificate.component.ts
+++ frontend/src/app/features/certificates/add-certificate/add-certificate.component.ts
@@ -3,7 +3,6 @@ import { BaseModalComponent } from '../../../shared/modal/base-modal.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BaseFormComponent } from '../../../shared/form/base-form.component';
 import { MatError, MatFormField, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
-import { provideI18nPrefix } from '../../../shared/i18n-prefix.provider';
 import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
 import { PctsFormErrorDirective } from '../../../shared/pcts-form-error/pcts-form-error.directive';
 import { PctsFormLabelDirective } from '../../../shared/pcts-form-label/pcts-form-label.directive';
@@ -15,7 +14,7 @@ import { CertificateTypeService } from '../certificate-type/certificate-type.ser
 import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle } from '@angular/material/datepicker';
 import { CertificateModel } from '../certificate.model';
 import { MemberModel } from '../../member/member.model';
-import { DialogResult, StrictlyTypedDialog } from '../../../shared/modal/strictly-typed-dialog.helper';
+import { DialogResult, provideModalI18nPrefix, StrictlyTypedDialog } from '../../../shared/modal/strictly-typed-dialog.helper';
 import { InputFieldComponent } from '../../../shared/input-field/input-field.component';
 import { ModalActionsComponent } from '../../../shared/modal/modal-actions.component';
 import { PCTSDialogConfig } from '../../../shared/modal/pcts-modal.service';
@@ -45,7 +44,7 @@ import { filterType } from '../../../shared/utils/typeFilter';
     ModalActionsComponent
   ],
   templateUrl: './add-certificate.component.html',
-  providers: [provideI18nPrefix('CERTIFICATE.FORM.ADD')]
+  providers: [provideModalI18nPrefix<CertificateModel>('CERTIFICATE.FORM.ADD', 'CERTIFICATE.FORM.EDIT')]
 })
 export class AddCertificateComponent extends StrictlyTypedDialog<PCTSDialogConfig<CertificateModel>, DialogResult<CertificateModel>> implements OnInit {
   private readonly fb = inject(FormBuilder);
