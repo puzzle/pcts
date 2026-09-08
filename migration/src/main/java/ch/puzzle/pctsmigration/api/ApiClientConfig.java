91e9ca7ffd72ed722efcf8d507644f84412cab34
Livio Canobbio <canobbio@puzzle.ch>
2026-09-08 11:21:35 +0200

    feat(backend): add leadershipex and type api client  #717

--- migration/src/main/java/ch/puzzle/pctsmigration/api/ApiClientConfig.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/api/ApiClientConfig.java
@@ -2,9 +2,7 @@ package ch.puzzle.pctsmigration.api;
 
 import ch.puzzle.pctsmigration.TokenSupplier;
 import org.openapitools.client.ApiClient;
-import org.openapitools.client.api.CertificateTypesApi;
-import org.openapitools.client.api.CertificatesApi;
-import org.openapitools.client.api.MembersApi;
+import org.openapitools.client.api.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +18,8 @@ public class ApiClientConfig {
         ApiClient apiClient = new ApiClient();
         apiClient.updateBaseUri(basePath);
         apiClient.setRequestInterceptor(builder -> builder.setHeader("Authorization", "Bearer " + tokenSupplier.get()));
+
+        System.out.println(tokenSupplier.get());
 
         return apiClient;
     }
@@ -37,5 +37,15 @@ public class ApiClientConfig {
     @Bean
     public CertificatesApi certificatesApi(ApiClient apiClient) {
         return new CertificatesApi(apiClient);
+    }
+
+    @Bean
+    public LeadershipExperiencesApi leadershipExperiencesApi(ApiClient apiClient) {
+        return new LeadershipExperiencesApi(apiClient);
+    }
+
+    @Bean
+    public LeadershipExperienceTypesApi leadershipExperienceTypesApi(ApiClient apiClient) {
+        return new LeadershipExperienceTypesApi(apiClient);
     }
 }
