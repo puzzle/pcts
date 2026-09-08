cc8cf228536f3592fa737eecb77a1fb301ac7821
schiltpzzle <schilt@puzzle.ch>
2026-09-08 15:10:42 +0200

    fix: patch values in ngOnInit in modals #798

--- frontend/src/app/features/degrees/add-degree/add-degree.component.ts
+++ frontend/src/app/features/degrees/add-degree/add-degree.component.ts
@@ -73,17 +73,8 @@ export class AddDegreeComponent extends StrictlyTypedDialog<PCTSDialogConfig<Deg
     comment: ['' as string | null]
   });
 
-  constructor() {
-    super();
-    if (this.data) {
-      this.formGroup.patchValue({
-        ...this.data.model
-      });
-    }
-  }
-
   ngOnInit(): void {
-    this.formGroup.patchValue(this.data ?? {});
+    this.formGroup.patchValue(this.data.model ?? {});
 
     this.degreeTypeService.getAllDegreeTypes()
       .subscribe((degreeTypes) => {
