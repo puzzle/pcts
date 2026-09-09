e3cc4b633492b3c636afcc50b72d8e805e961a25
Livio Canobbio <canobbio@puzzle.ch>
2026-09-09 15:20:45 +0200

    feat(backend): expand odsparser with startMarker and RowCollector to regulate which rows will extracted  #717

--- migration/src/main/java/ch/puzzle/pctsmigration/leadershipexperience/LeadershipExperienceWrapper.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/leadershipexperience/LeadershipExperienceWrapper.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record LeadershipExperienceWrapper (@Valid @NotNull List<LeadershipExperienceAiResultDto> items) {
+public record LeadershipExperienceWrapper(@Valid @NotNull List<LeadershipExperienceAiResultDto> items) {
 
 }
