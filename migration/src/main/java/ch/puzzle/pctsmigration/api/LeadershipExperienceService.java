cc8188d775552add8f7a809e65476a934e24c058
Livio Canobbio <canobbio@puzzle.ch>
2026-09-11 11:14:56 +0200

    refactor: outsource the create logic into abstract class  #717

--- migration/src/main/java/ch/puzzle/pctsmigration/api/LeadershipExperienceService.java
+++ migration/src/main/java/ch/puzzle/pctsmigration/api/LeadershipExperienceService.java
@@ -1,49 +1,31 @@
 package ch.puzzle.pctsmigration.api;
 
-import ch.puzzle.pctsmigration.exception.Error;
-import ch.puzzle.pctsmigration.exception.MigrationException;
-import java.util.ArrayList;
-import java.util.List;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.api.LeadershipExperiencesApi;
 import org.openapitools.client.model.LeadershipExperienceDto;
 import org.openapitools.client.model.LeadershipExperienceInputDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LeadershipExperienceService {
+public class LeadershipExperienceService extends CreationBase<LeadershipExperienceInputDto, LeadershipExperienceDto> {
     private final LeadershipExperiencesApi leadershipExperiencesApi;
-    private final Logger logger = LoggerFactory.getLogger(LeadershipExperienceService.class);
 
     public LeadershipExperienceService(LeadershipExperiencesApi leadershipExperiencesApi) {
         this.leadershipExperiencesApi = leadershipExperiencesApi;
     }
 
-    public void create(List<LeadershipExperienceInputDto> dtos) {
-        List<Long> createdIds = new ArrayList<>();
-
-        for (LeadershipExperienceInputDto dto : dtos) {
-            try {
-                LeadershipExperienceDto created = this.leadershipExperiencesApi.createLeadershipExperience(dto);
-                createdIds.add(created.getId());
-            } catch (ApiException e) {
-                rollbackCreatedLeadershipExperiences(createdIds);
-                throw new MigrationException(new Error(HttpStatusCode.valueOf(400),
-                                                       "Migration aborted. Reason: " + e.getMessage()));
-            }
-        }
+    @Override
+    protected LeadershipExperienceDto executeCreate(LeadershipExperienceInputDto dto) throws ApiException {
+        return this.leadershipExperiencesApi.createLeadershipExperience(dto);
     }
 
-    private void rollbackCreatedLeadershipExperiences(List<Long> createdIds) {
-        for (Long id : createdIds) {
-            try {
-                this.leadershipExperiencesApi.deleteLeadershipExperience(id);
-            } catch (ApiException rollbackException) {
-                this.logger.error("Rollback failed for ID {}", id, rollbackException);
-            }
-        }
+    @Override
+    protected void executeDelete(Long id) throws ApiException {
+        this.leadershipExperiencesApi.deleteLeadershipExperience(id);
+    }
+
+    @Override
+    protected Long extractId(LeadershipExperienceDto entity) {
+        return entity.getId();
     }
 }
