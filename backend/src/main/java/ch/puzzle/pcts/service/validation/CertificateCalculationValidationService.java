a00cb630aa58e621aab6695e228321dd22d812a6
carranza-javier <kettenki@gmail.com>
2026-09-11 13:13:20 +0200

    refactor: abstract calculation child validation #410

--- backend/src/main/java/ch/puzzle/pcts/service/validation/CertificateCalculationValidationService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/validation/CertificateCalculationValidationService.java
@@ -1,21 +1,13 @@
 package ch.puzzle.pcts.service.validation;
 
-import static ch.puzzle.pcts.Constants.CALCULATION;
+import static ch.puzzle.pcts.Constants.CERTIFICATE;
 
-import ch.puzzle.pcts.dto.error.ErrorKey;
-import ch.puzzle.pcts.dto.error.FieldKey;
-import ch.puzzle.pcts.dto.error.GenericErrorDto;
-import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.model.calculation.certificatecalculation.CertificateCalculation;
-import ch.puzzle.pcts.model.member.Member;
-import ch.puzzle.pcts.service.validation.util.CalculationChildValidationUtil;
 import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CertificateCalculationValidationService extends ValidationBase<CertificateCalculation> {
+public class CertificateCalculationValidationService extends CalculationChildValidationBase<CertificateCalculation> {
 
     @Override
     public void validateOnCreate(CertificateCalculation model) {
@@ -31,36 +23,15 @@ public class CertificateCalculationValidationService extends ValidationBase<Cert
 
     public void validateDuplicateCertificateId(CertificateCalculation certificateCalculation,
                                                List<CertificateCalculation> certificateCalculationList) {
-        if (CalculationChildValidationUtil
-                .validateDuplicateCalculationChildId(certificateCalculation, certificateCalculationList)) {
-            Map<FieldKey, String> attributes = Map
-                    .of(FieldKey.ENTITY,
-                        CALCULATION,
-                        FieldKey.FIELD,
-                        "certificate",
-                        FieldKey.IS,
-                        certificateCalculation.getCertificate().getCertificateType().getName());
-
-            throw new PCTSException(HttpStatus.BAD_REQUEST,
-                                    List.of(new GenericErrorDto(ErrorKey.DUPLICATE_CALCULATION, attributes)));
-        }
+        validateNoDuplicate(certificateCalculation,
+                            certificateCalculationList,
+                            CERTIFICATE,
+                            calculationChild -> calculationChild.getCertificate().getCertificateType().getName());
     }
 
     public void validateMemberForCalculation(CertificateCalculation model) {
-        Member certificateMember = model.getCertificate().getMember();
-        Member calculationMember = model.getCalculation().getMember();
-
-        if (!certificateMember.equals(calculationMember)) {
-            Map<FieldKey, String> attributes = Map
-                    .of(FieldKey.ENTITY,
-                        CALCULATION,
-                        FieldKey.FIELD,
-                        "certificate",
-                        FieldKey.CONDITION_FIELD,
-                        "member");
-
-            GenericErrorDto error = new GenericErrorDto(ErrorKey.ATTRIBUTE_DOES_NOT_MATCH, attributes);
-            throw new PCTSException(HttpStatus.BAD_REQUEST, List.of(error));
-        }
+        validateMemberMatchesCalculation(model,
+                                         CERTIFICATE,
+                                         calculationChild -> calculationChild.getCertificate().getMember());
     }
 }
