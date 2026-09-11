116473a399548db6f87ae0b4d310128ff2e95a4d
carranza-javier <kettenki@gmail.com>
2026-09-11 13:13:20 +0200

    refactor: abstract calculation child validation #410

--- backend/src/main/java/ch/puzzle/pcts/service/validation/DegreeCalculationValidationService.java
+++ backend/src/main/java/ch/puzzle/pcts/service/validation/DegreeCalculationValidationService.java
@@ -1,21 +1,13 @@
 package ch.puzzle.pcts.service.validation;
 
-import static ch.puzzle.pcts.Constants.CALCULATION;
+import static ch.puzzle.pcts.Constants.DEGREE;
 
-import ch.puzzle.pcts.dto.error.ErrorKey;
-import ch.puzzle.pcts.dto.error.FieldKey;
-import ch.puzzle.pcts.dto.error.GenericErrorDto;
-import ch.puzzle.pcts.exception.PCTSException;
 import ch.puzzle.pcts.model.calculation.degreecalculation.DegreeCalculation;
-import ch.puzzle.pcts.model.member.Member;
-import ch.puzzle.pcts.service.validation.util.CalculationChildValidationUtil;
 import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DegreeCalculationValidationService extends ValidationBase<DegreeCalculation> {
+public class DegreeCalculationValidationService extends CalculationChildValidationBase<DegreeCalculation> {
     @Override
     public void validateOnCreate(DegreeCalculation model) {
         super.validateOnCreate(model);
@@ -30,31 +22,13 @@ public class DegreeCalculationValidationService extends ValidationBase<DegreeCal
 
     public void validateDuplicateDegreeId(DegreeCalculation degreeCalculation,
                                           List<DegreeCalculation> degreeCalculationList) {
-        if (CalculationChildValidationUtil
-                .validateDuplicateCalculationChildId(degreeCalculation, degreeCalculationList)) {
-            Map<FieldKey, String> attributes = Map
-                    .of(FieldKey.ENTITY,
-                        CALCULATION,
-                        FieldKey.FIELD,
-                        "degree",
-                        FieldKey.IS,
-                        degreeCalculation.getDegree().getName());
-
-            GenericErrorDto error = new GenericErrorDto(ErrorKey.DUPLICATE_CALCULATION, attributes);
-            throw new PCTSException(HttpStatus.BAD_REQUEST, List.of(error));
-        }
+        validateNoDuplicate(degreeCalculation,
+                            degreeCalculationList,
+                            DEGREE,
+                            calculationChild -> calculationChild.getDegree().getName());
     }
 
     public void validateMemberForCalculation(DegreeCalculation model) {
-        Member degreeMember = model.getDegree().getMember();
-        Member calculationMember = model.getCalculation().getMember();
-
-        if (!degreeMember.equals(calculationMember)) {
-            Map<FieldKey, String> attributes = Map
-                    .of(FieldKey.ENTITY, CALCULATION, FieldKey.FIELD, "degree", FieldKey.CONDITION_FIELD, "member");
-
-            GenericErrorDto error = new GenericErrorDto(ErrorKey.ATTRIBUTE_DOES_NOT_MATCH, attributes);
-            throw new PCTSException(HttpStatus.BAD_REQUEST, List.of(error));
-        }
+        validateMemberMatchesCalculation(model, DEGREE, calculationChild -> calculationChild.getDegree().getMember());
     }
 }
