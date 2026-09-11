6c3464f24a21bf9fe7e8e4d8e36c84fc6b9a65c1
schiltpzzle <schilt@puzzle.ch>
2026-09-11 10:23:47 +0200

    fix: refactor memberId resolver #798

--- frontend/src/app/features/member/member-id-resolver.ts
+++ frontend/src/app/features/member/member-id-resolver.ts
@@ -1,8 +1,7 @@
 import { ResolveFn } from '@angular/router';
 
 export const memberIdResolver: ResolveFn<number> = (route): number => {
-  const id: string | undefined = route.paramMap.get('id')
-    ?.split('?')[0];
+  const id: string | null = route.paramMap.get('id');
 
   return id ? Number(id) : 0;
 };
