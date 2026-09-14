68e8791f3d8dd5f8901949362a50fe0c579e1b11
schiltpzzle <schilt@puzzle.ch>
2026-09-14 11:57:33 +0200

    fix: throw error instead of returning zero as member id #798

--- frontend/src/app/features/member/member-id-resolver.ts
+++ frontend/src/app/features/member/member-id-resolver.ts
@@ -3,5 +3,9 @@ import { ResolveFn } from '@angular/router';
 export const memberIdResolver: ResolveFn<number> = (route): number => {
   const id: string | null = route.paramMap.get('id');
 
-  return id ? Number(id) : 0;
+  if (!id) {
+    throw new Error('Member id is missing in the route');
+  }
+
+  return Number(id);
 };
