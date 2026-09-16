08e348ac19277047ad181c8e28dac7ba7615d2da
Thirusiga <tthiruarulchelvan@puzzle.ch>
2026-09-16 14:51:06 +0200

    test(frontend): add test for delete button  #799

--- frontend/cypress/e2e/modal-actions.cy.ts
+++ frontend/cypress/e2e/modal-actions.cy.ts
@@ -1,42 +1,36 @@
-import * as users from '../fixtures/users.json';
+// Only one preparation file will be deleted later when the other modals are finished and the tests have been copied over and changed to fit
+import modalPage from '../pages/modalPage';
 import memberDetailPage from '../pages/memberDetailPage';
+import * as users from '../fixtures/users.json';
+import formPage from '../pages/formPage';
 
-describe('ModalActions', () => {
+describe('Degree Modal', () => {
   beforeEach(() => {
     cy.loginAsUser(users.gl);
     memberDetailPage.visit(1);
   });
 
-  it('should open the edit modal and show the modal actions', () => {
+  describe('Edit degree modal', () => {
+    const openDegreeModal = () => {
+      cy.getByTestId('cv-table-degree')
+        .within(() => {
+          cy.getByTestId('generic-table-cell')
+            .eq(1)
+            .click();
+        });
 
-  });
-  it('should display the cancel button', () => {
-    cy.contains('button', 'Cancel')
-      .should('be.visible');
-  });
+      modalPage.checkModalIconButtonVisible();
+    };
 
-  it('should display the submit button', () => {
-    cy.contains('button', 'Action')
-      .should('be.visible');
-  });
+    beforeEach(() => {
+      openDegreeModal();
+    });
 
-  it('should display the delete button', () => {
-    cy.contains('button', 'Delete')
-      .should('be.visible');
-  });
-
-  it('should click cancel', () => {
-    cy.contains('button', 'Cancel')
-      .click();
-  });
-
-  it('should click submit', () => {
-    cy.contains('button', 'Action')
-      .click();
-  });
-
-  it('should click delete', () => {
-    cy.contains('button', 'Delete')
-      .click();
+    it('should open the edit modal and delete the modal', () => {
+      cy.getByTestId('delete-button')
+        .click();
+      formPage.shouldShowSuccessToast('Ausbildung wurde erfolgreich gelöscht.');
+      modalPage.checkModalIsClosed();
+    });
   });
 });
