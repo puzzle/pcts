561608d60cd476b62ecb50b6afa5034bb31e0ee7
Flynn Streit <streit@puzzle.ch>
2026-09-18 10:07:01 +0200

    test(frontend): create e2e for editing certificate modal  #798

--- frontend/cypress/e2e/add-certificate.cy.ts
+++ frontend/cypress/e2e/add-certificate.cy.ts
@@ -112,4 +112,59 @@ describe('Add certificate Modal', () => {
       });
     });
   });
+  describe('Edit certificate modal', () => {
+    const openCertificateModal = () => {
+      cy.getByTestId('cv-table-certificate')
+        .within(() => {
+          cy.getByTestId('generic-table-cell')
+            .eq(1)
+            .click();
+        });
+    };
+
+    beforeEach(() => {
+      openCertificateModal();
+    });
+
+    it('should open correct modal', () => {
+      modalPage.modalTitle()
+        .should('include.text', 'Zertifikat bearbeiten');
+    });
+
+    it('should save changes correctly', () => {
+      cy.intercept('PUT', 'api/v1/certificates/**')
+        .as('certificates');
+
+      formPage.clearAndBlur('certificateType');
+      modalPage.selectAutoCompleteValue('certificateType', 'Microsoft Certified: Azure Administrator Associate');
+
+      formPage.clearAndBlur('completedAt');
+      formPage.typeAndBlur('completedAt', '10.10.2023');
+
+      formPage.clearAndBlur('validUntil');
+      formPage.typeAndBlur('validUntil', '10.12.2027');
+
+      formPage.clearAndBlur('comment');
+      formPage.typeAndBlur('comment', 'This is a comment.');
+
+      cy.getByTestId('submit-button')
+        .click();
+
+      formPage.shouldShowSuccessToast('Zertifikat wurde erfolgreich aktualisiert.');
+
+      cy.wait('@certificates');
+
+      cy.get('@certificates')
+        .then((interception) => {
+          expect(interception.request.body).to.contain({
+            memberId: 1,
+            certificateTypeId: 10,
+            completedAt: '2023-10-10',
+            validUntil: '2027-12-10',
+            comment: 'This is a comment.'
+          });
+        });
+      modalPage.checkModalIsClosed();
+    });
+  });
 });
