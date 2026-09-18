c7fc832d11745b5dccc1ced5dab24ed931745940
schiltpzzle <schilt@puzzle.ch>
2026-09-18 10:08:38 +0200

    test: add test for member id resolver #798

--- frontend/src/app/features/member/member-id-resolver.spec.ts
+++ frontend/src/app/features/member/member-id-resolver.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-import { ResolveFn } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { memberIdResolver } from './member-id-resolver';
 
 describe('memberIdResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => TestBed.runInInjectionContext(() => memberIdResolver(...resolverParameters));
+  let mockRoute: ActivatedRouteSnapshot;
+  let mockState: RouterStateSnapshot;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockRoute = {
+      paramMap: {
+        get: jest.fn()
+      }
+    } as unknown as ActivatedRouteSnapshot;
+
+    mockState = {} as RouterStateSnapshot;
   });
 
-  it('should be created', () => {
-    expect(executeResolver)
-      .toBeTruthy();
+  it('should return the id as number', () => {
+    const id = 1;
+    (mockRoute.paramMap.get as jest.Mock).mockReturnValue(id.toString());
+
+    const result = memberIdResolver(mockRoute, mockState);
+
+    expect(mockRoute.paramMap.get)
+      .toHaveBeenCalledTimes(1);
+    expect(result)
+      .toEqual(id);
+  });
+
+  it('should throw a error if id is missing', () => {
+    (mockRoute.paramMap.get as jest.Mock).mockReturnValue(null);
+
+    expect(() => memberIdResolver(mockRoute, mockState))
+      .toThrow('Member id is missing in the route');
   });
 });
