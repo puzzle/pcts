faf5b2c90160ec9c5dd0a009d85644c455266a99
Flynn Streit <streit@puzzle.ch>
2026-09-21 11:51:52 +0200

    test(frontend): make test use the new output signal syntax  #799

--- frontend/src/app/shared/modal/modal-actions.component.spec.ts
+++ frontend/src/app/shared/modal/modal-actions.component.spec.ts
@@ -31,7 +31,7 @@ describe('ModalActionsComponent', () => {
     it('should trigger the cancel event when onCancel() is called', () => {
       const cancelSpy = jest.spyOn(component.cancelAction, 'emit');
 
-      component.onCancel();
+      component.handleCancel();
 
       expect(cancelSpy)
         .toHaveBeenCalledTimes(1);
@@ -41,7 +41,7 @@ describe('ModalActionsComponent', () => {
       const submitSpy = jest.spyOn(component.submitAction, 'emit');
       const testMode = ModalSubmitMode.SAVE;
 
-      component.onSubmit(testMode);
+      component.handleSubmit(testMode);
 
       expect(submitSpy)
         .toHaveBeenCalledTimes(1);
@@ -52,7 +52,7 @@ describe('ModalActionsComponent', () => {
     it('should fire the correct mode when onDelete() is called', () => {
       const deleteSpy = jest.spyOn(component.deleteAction, 'emit');
 
-      component.deleteAction.emit();
+      component.handleDelete();
 
       expect(deleteSpy)
         .toHaveBeenCalledTimes(1);
