007aea32edfdf8b5ed13dcec3025d6372a0c628c
schiltpzzle <schilt@puzzle.ch>
2026-09-24 14:04:53 +0200

    feat: maek directive typed #914

--- frontend/src/app/shared/generic-table/rowDetailTemplate.directive.ts
+++ frontend/src/app/shared/generic-table/rowDetailTemplate.directive.ts
@@ -1,9 +1,17 @@
-import { Directive, inject, TemplateRef } from '@angular/core';
+import { Directive, inject, input, TemplateRef } from '@angular/core';
+import { GenericTableDataSource } from './generic-table-data-source';
 
 @Directive({
   selector: '[appExpandableRow]',
   standalone: true
 })
-export class RowDetailTemplateDirective {
+export class RowDetailTemplateDirective<T> {
+  appExpandableRow = input.required<GenericTableDataSource<T>>();
+
   template = inject(TemplateRef);
+
+  static ngTemplateContextGuard<TContext>(dir: RowDetailTemplateDirective<TContext>,
+    ctx: unknown): ctx is { $implicit: TContext } {
+    return true;
+  }
 }
