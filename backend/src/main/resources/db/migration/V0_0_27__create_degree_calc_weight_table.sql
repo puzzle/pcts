b5a69c30b8f81f44d5f1b887e7f080aa478ec59c
Thirusiga <tthiruarulchelvan@puzzle.ch>
2026-09-25 10:11:15 +0200

    refactor(db): made weight can only be 0-100 with smallint  #904

--- backend/src/main/resources/db/migration/V0_0_27__create_degree_calc_weight_table.sql
+++ backend/src/main/resources/db/migration/V0_0_27__create_degree_calc_weight_table.sql
@@ -1,6 +1,6 @@
 Create TABLE degree_calculation_weight (
                                            id BIGINT GENERATED ALWAYS As IDENTITY PRIMARY KEY NOT NULL,
-                                           weight NUMERIC NOT NULL,
+                                           weight SMALLINT NOT NULL CHECK (weight BETWEEN 0 AND 100),
                                            relevancy TEXT NOT NULL,
                                            degree_calculation_id BIGINT NOT NULL,
                                            UNIQUE (degree_calculation_id, relevancy),
